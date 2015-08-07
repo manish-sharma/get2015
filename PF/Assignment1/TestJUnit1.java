ee095c54000d2a54bb5f1e890df9650032615143
meta-neha-bansal <neha.bansal@metacube.com>
2015-08-07 15:48:17 +0530

    PF second assignment

--- PF/Assignment1/TestJUnit1.java
+++ PF/Assignment1/TestJUnit1.java
@@ -1,25 +1,14 @@
-
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-public class TestJUnit1 {
+import static org.junit.Assert.assertEquals;
+public class TestJUnit {
 	
-	PF_assignment2_Q1 pf=new PF_assignment2_Q1();
-	String spaceString=pf.spaces(1,3);
-	String numberString=pf.numbers(3,3);
-	String str=pf.pyramid(3);
-	
-	
-	String expected1="  ";
-	String expected2="12321";
-	String expected3="  1\n 121\n12321\n 121\n  1";
-
+   String message = "Hello World";	
+   Binary2octal b1=new Binary2octal();
 	
    @Test
    public void testPrintMessage() {
       
-	   assertEquals(spaceString,expected1);
-	   assertEquals(numberString,expected2);
-	   assertEquals(str,expected3);
+      assertEquals(65,b1.convertBinaryToOctal(110101));
    }
 }
--- PF/Assignment1/TestJUnit2.java
+++ PF/Assignment1/TestJUnit2.java
@@ -1,24 +1,21 @@
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-public class TestJUnit2 {
+
+import static org.junit.Assert.assertEquals;
+public class TestJUnit {
 	
-	PF_assignment2_Q2 pf=new PF_assignment2_Q2();
-	
-	String spaceString=pf.spaces(1,3);
-	String numberString=pf.numbers(1,5);
-	String str=pf.pyramid(3);
-	
-	
-	String expected1="";
-	String expected2="12345";
-	String expected="123\n 12\n  1\n";
+	RemoveDuplicate rm=new RemoveDuplicate();
+	int arr[]=new int[]{3,2,2,6,7,7,6,1,2,3};
+	//System.out.println("output is"+arr.length);
+	int op[]=rm.removeDuplicat(arr);
+	int expected[]=new int[]{3,2,6,7,1};
+	//int a=b1.convertBinaryToOctal(110101);
+	//System.out.println("Output is "+a);
+
    @Test
    public void testPrintMessage() {
       
-	   assertEquals(spaceString,expected1);
-	   assertEquals(numberString,expected2);
-	   assertEquals(expected,str);
+	   assertArrayEquals(expected,op);
    }
 }
