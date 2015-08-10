206de1b43defa931239935397402acba78ef10ea
meta-babalu-patidar <babalu.patidar@metacube.com>
2015-08-10 12:00:00 +0530

    Update Assignment2.java

--- PF/Assignment2/Assignment2.java
+++ PF/Assignment2/Assignment2.java
@@ -1,7 +1,16 @@
+/*************************
+ * @author Babalu Patidar
+ * Program to draw a pyramid pattern
+ * spaces method is used for return space string
+ * numbers method is used for return number string  
+ * drawpyramid method is used for draw pattern with the help of spaces and numbers method
+ ***********/
 
+
+/* Starting of class*/
 public class Assignment2 {
 	
-
+/* Starting of spaces method*/
 	public String  spaces(int row, int n)// it will calculate no. of spaces in row
 	{
 		String output="";
@@ -13,8 +22,10 @@ public class Assignment2 {
 		
 		return output;
 		
-	}
+	}/* Ending of spaces method*/
 	
+	
+	/* Starting of numbers method*/
 	public String numbers(int row, int n)// it will calculate number string in row
 	{
 		String output="";
@@ -25,8 +36,10 @@ public class Assignment2 {
 			output+=j;
 			}
 		return output;
-	}
+	}/* Ending of numbers method*/
 	
+	
+	/* Starting of drawpyramid method*/
 	public String[] drawpyramid(int n)// it will draw pyramid by help of space and numbers method 
 	{
 		
@@ -42,15 +55,17 @@ public class Assignment2 {
 		}
 		return output;  //return pyramid as a Array Of String  
 
-	}
+	}/* Ending of drawpyramid method*/
 	
+	
+	/* Starting of main method*/
 	public static void main (String arg[])
 	{
 		Assignment2 b1=new Assignment2();
 		String out[]=b1.drawpyramid(3);
 		for(int i=0;i<3;i++)
 		System.out.println(out[i]);
-	}
+	}/* Ending of main method*/
 
 
-}
+}/*Ending of class*/
