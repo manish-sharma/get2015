12410af8aaf189640b9490fae306a8daeacaa9aa
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-12 16:50:48 +0530

    Create TestForTowerOfHonai.java
    Test code to check tower of honai

---
+++ PF/Assignment4/TestForTowerOfHonai.java
@@ -0,0 +1,89 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestForTowerOfHonai {
+
+	TowerOfHonai tower=new TowerOfHonai();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void Test()
+	{
+		tower.size=1;
+		tower.result=new ArrayList<String>();
+		tower.result=tower.honai("A", "B", "C", 1);
+		String expected[]={"Move Disk 1 from A to B"};
+		
+		
+		int i=0;
+		
+		for(String a:tower.result)
+		{
+			
+			assertEquals(expected[i],a);
+			i++;
+		}
+		
+	}
+	@Test
+	public void TestForTwo()
+	{
+		tower.size=2;
+		tower.result=new ArrayList<String>();
+		tower.result=tower.honai("A", "B", "C", 1);
+		String expected[]={"Move Disk 2 from A to C","Move Disk 1 from A to B","Move Disk 2 from C to B"};
+		
+		
+		int i=0;
+		
+		for(String a:tower.result)
+		{
+			
+			assertEquals(expected[i],a);
+			i++;
+		}
+		
+	}
+	@Test
+	public void TestForThree()
+	{
+		tower.size=3;
+		tower.result=new ArrayList<String>();
+		tower.result=tower.honai("A", "B", "C", 1);
+		String expected[]={"Move Disk 3 from A to B","Move Disk 2 from A to C","Move Disk 3 from B to C","Move Disk 1 from A to B","Move Disk 3 from C to A","Move Disk 2 from C to B","Move Disk 3 from A to B"};
+		
+		
+		int i=0;
+		
+		for(String a:tower.result)
+		{
+			
+			assertEquals(expected[i],a);
+			i++;
+		}
+		
+	}
+
+}
