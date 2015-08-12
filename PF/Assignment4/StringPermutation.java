0a3587af6a60dcd480aadaa9e8d3d6e73db73909
banwari kevat <banwari.kevat@metacube.com>
2015-08-12 16:28:39 +0530

    Addded All Assignments

--- PF/Assignment4/StringPermutation.java
+++ PF/Assignment4/StringPermutation.java
@@ -1,13 +1,20 @@
+/** 
+   *   class for To generate all possible permutation of an given string
+*/
 class StrPermu{
+	//res store all permutation string
 	private String[] res;
+	// index for res
 	private int index;
 	
 	StrPermu(int size)
 	{
-		res= new String[(int)Math.pow(2,size)-1];
+		// decision for how many permutations will be generated 
+		res= new String[(int)Math.pow(2,size)-2];
 		index=0;
 	}
 	
+	// This function take string and convert it into array then call for permute()
 	public void generatePermutations(String str) {
 		char [] strArray= str.toCharArray();
 		int charinstr= strArray.length;		
@@ -17,30 +24,42 @@ class StrPermu{
 		
 	}
 	
+//  this is recursive function
 	void permute(char arr[], int lowi, int highi)
 	{
 	   int i;
 	   char temp;
 	   
 	   if (lowi == highi){
-	           res[index]=arr.toString();
+	           res[index]=arrayToString(arr);
 	           index++;
 	     }
 	   else
-	   {
+	   {     
 	       for (i = lowi; i <=highi; i++)
 	       {
-	          temp=arr[i]; arr[i]=arr[lowi]; arr[lowi]=temp;                                 //swap(arr[lowi], arr[i]);
+	    	   //swap(arr[lowi], arr[i]);
+	    	   temp=arr[i]; arr[i]=arr[lowi]; arr[lowi]=temp;                                 //swap(arr[lowi], arr[i]);
 	          permute(arr, lowi+1, highi);
+	          //swap(arr[lowi], arr[i]);
 	          temp=arr[i]; arr[i]=arr[lowi]; arr[lowi]=temp;                                  //swap(arr[lowi], arr[i]) //backtrack
 	       }
 	   }
 	}
 	
-	
+	// this method returns the final Array of permutations
 	String [] storeAllPermutation(){
 	
 		return this.res;
+	}
+	
+	
+	//convert array to string
+	String arrayToString(char[] arr){
+		String str="";
+		for(int i=0;i<arr.length;i++)
+		      { str= str+arr[i];}
+	    return str;	
 	}
 	
 };
@@ -53,8 +72,8 @@ public class StringPermutation {
        
         String[] result=obj.storeAllPermutation();
         
-        for(String i: result) {
-        	System.out.println(i);
+        for(int i=0;i<result.length;i++) {
+        	System.out.println(result[i]);
         }
       
        
