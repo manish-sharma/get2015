622d610f7960fe1f1d695d05226dec1488185731
meta-anjita-maheshwari <anjita.maheshwari@metacube.com>
2015-08-12 20:50:01 +0530

    Update Assignment2.java

--- PF/Assignment2/Assignment2.java
+++ PF/Assignment2/Assignment2.java
@@ -1,42 +1,42 @@
 package Assignments;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
+//Class for finding permutation of elements of string
 public class Assignment2 {
+	static List<String> result; // Object of List String type is declared
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Assignment2 a1=new Assignment2();
-System.out.println("enter size of pyramid");
-Scanner sc = new Scanner(System.in);
-int num = sc.nextInt();
-String result[]=new String[num];
-		
-	
-		result=a1.printWholePattern(num);
-		for(int i=0;i<num;i++)
-		System.out.println(result[i]);
+		Assignment2 permutation = new Assignment2();
+		result = new ArrayList<String>();
+		System.out.println("Enter the string");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		result = permutation.generatePermutations("", str);
+		Iterator<String> itr = result.iterator(); // getting Iterator from
+													// arraylist to traverse
+													// elements
+		while (itr.hasNext()) { // hasNext() gives value true if next string is
+								// present in the list
+			System.out.println(itr.next()); // next() returns the next string in list
+		}
+
 	}
-String patternSpace(int row,int n)
-{
-	String s1="";
-	for(int i=0;i<row;i++)
-		s1=s1+" ";
-	return s1;
-}
-String patternNumber(int row,int n)
-{
-	String s2="";
-	for(int i=1;i<=(n-row);i++)
-		s2=s2+i;
-	return s2;
-}
-String[] printWholePattern(int n)
-{
-	String finalLine[]=new String[n];
-	for(int i=0;i<n;i++)
-		finalLine[i]=patternSpace(i,n)+patternNumber(i,n);
-	return finalLine;
-}
+
+	List<String> generatePermutations(String prefix, String input) // Function for finding permutation of elements of string
+	{
+		int n = input.length();
+		if (n == 0)
+			result.add(prefix);
+		else {
+			for (int i = 0; i < n; i++)
+				generatePermutations(prefix + input.charAt(i),
+						input.substring(0, i) + input.substring(i + 1, n));
+		}
+		return result;
+
+	}
 }
