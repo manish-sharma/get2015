f56ed413608cfaa6742944beeb4adbd6a8cf18d9
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-13 19:03:40 +0530

    Create Square.java

---
+++ PF/OOPAssignment1/Square.java
@@ -0,0 +1,17 @@
+
+public class Square implements Shape {
+	private int side;
+	public Square(int side){this.side = side;}
+	public void draw(){
+		// Square drawing logic
+		System.out.println("this is Square");
+		}
+	
+	public double getArea(){
+		// Square area calculation
+		double area=side*side;
+		return area;
+		}
+	}
+
+
