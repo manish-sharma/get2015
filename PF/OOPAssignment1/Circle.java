732b525e0de5a139407fa4d85c1b644cf8d29d00
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-13 19:00:50 +0530

    Create Circle.java

---
+++ PF/OOPAssignment1/Circle.java
@@ -0,0 +1,16 @@
+
+public class Circle implements Shape {
+	private int radius;
+	public Circle(int radius){this.radius = radius;}
+	public void draw(){
+	// Circle drawing logic
+		System.out.println("this is circle");
+	}
+	public double getArea(){
+	// Circle area calculation
+		double area=3.14*radius*radius;
+		return area;
+	}
+
+
+}
