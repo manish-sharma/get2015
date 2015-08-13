c190d697b940be1b61bb4cdbc046f16b0132e078
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-13 19:04:15 +0530

    Create Student.java

---
+++ PF/OOPAssignment1/Student.java
@@ -0,0 +1,16 @@
+public class Student extends Person {
+private int studentId;
+private String[] courses;
+public Student(int uid, String name, int studentId) {
+super(uid,name);
+this.studentId = studentId;
+}
+public int getStudentId() {return studentId;}
+public void setStudentId(int studentId) {this.studentId =
+studentId;}
+public String[] getCourses() {return courses;}
+public void setCources(String[] courses) {this.courses = courses;}
+public String toString(){
+return super.toString() + ", StudentId:"+studentId;
+}
+}
