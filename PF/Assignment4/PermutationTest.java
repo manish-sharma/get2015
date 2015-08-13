f92d31c4711308e9befa3fc7198f4932077ba113
meta-udit-nagar <udit.nagar@metacube.com>
2015-08-13 19:11:15 +0530

    Update PermutationTest.java

--- PF/Assignment4/PermutationTest.java
+++ PF/Assignment4/PermutationTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,22 +46,19 @@ public class PermutationTest {
 	 */
 	@Test
 	public void permutationTest1() {
-		List<String> list = new ArrayList<String>();
-		List<String> resultString = Permutation.permuteString("", "abcd", list);
+
+		List<String> resultString = Permutation.permuteStrings("", "abcd");
 		List<String> expectedString1 = Arrays.asList("abcd", "abdc", "acbd",
 				"acdb", "adbc", "adcb", "bacd", "badc", "bcad", "bcda", "bdac",
 				"bdca", "cabd", "cadb", "cbad", "cbda", "cdab", "cdba", "dabc",
 				"dacb", "dbac", "dbca", "dcab", "dcba");
 		assertEquals(expectedString1, resultString);
 
-		List<String> list1 = new ArrayList<String>();
-		List<String> resultString1 = Permutation.permuteString("", "ab", list1);
+		List<String> resultString1 = Permutation.permuteStrings("", "ab");
 		List<String> expectedString2 = Arrays.asList("ab", "ba");
 		assertEquals(expectedString2, resultString1);
 
-		List<String> list2 = new ArrayList<String>();
-		List<String> resultString2 = Permutation
-				.permuteString("", "abc", list2);
+		List<String> resultString2 = Permutation.permuteStrings("", "abc");
 		List<String> expectedString3 = Arrays.asList("abc", "acb", "bac",
 				"bca", "cab", "cba");
 
