2c610b29a6498cf4c8f00f7f73e6d82495884034
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-14 12:10:56 +0530

    Update Person.java
    Source code for person parent class

--- PF/OOPAssignment1/Person.java
+++ PF/OOPAssignment1/Person.java
@@ -1,15 +1,58 @@
-public class Person {
-private int uid;
-private String name;
-public Person(int uid, String name){
-this.uid = uid;
-this.name = name;
-}
-public int getUid() {return uid;}
-public void setUid(int uid) {this.uid = uid;}
-public String getName() {return name;}
-public void setName(String name) {this.name = name;}
-public String toString(){
-return "Name:"+name+", uid:"+uid;
-}
+/**
+ * @author Sumitra
+ * this program is to set whole record for  person class
+ */
+public class Person 
+{
+	private int uid;
+	private String name;
+	/**
+	 * 
+	 * @param uid user id is set in parameterized constructor
+	 * @param name name is set in parameterized constructor
+	 */
+	public Person(int uid, String name)
+	{
+		this.uid = uid;
+		this.name = name;
+	}
+	/**
+	 * 
+	 * @return will return the usedId of person
+	 */
+	public int getUid()
+	{
+		return uid;
+	}
+	/**
+	 * 
+	 * @param uid will set user id for person
+	 */
+	public void setUid(int uid)
+	{
+		this.uid = uid;
+	}
+	/**
+	 * 
+	 * @return will return the name of person
+	 */
+	public String getName() 
+	{
+		return name;
+	}
+	/**
+	 * 
+	 * @param name will set the name
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	/**
+	 * will return the whole data of person
+	 */
+	public String toString()
+	{
+		return "Name:"+name+", uid:"+uid;
+	}
 }
