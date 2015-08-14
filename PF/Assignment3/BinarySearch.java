7279e1f01c30b5dec1bed531850a3934e14b1afa
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:42:43 +0530

    Update and rename Prg2b.java to BinarySearch.java

---
+++ PF/Assignment3/BinarySearch.java
@@ -0,0 +1,50 @@
+package assignment3;
+
+import java.util.Scanner;
+
+//program for binary search
+public class BinarySearch {
+	private static Scanner in;
+
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int size = in.nextInt();
+		System.out.println("Enter an array of numbers: ");
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = in.nextInt();
+		}
+
+		System.out.println("Enter number you want to search: ");
+		int num = in.nextInt();
+		BinarySearch obj = new BinarySearch();
+		int start = 0;
+		int end = arr.length - 1;
+		int index = obj.binary(arr, num, start, end);
+		if (index > 0) {
+			System.out.println("Number you wanted to search is at "
+					+ (index + 1));
+
+		} else {
+			System.out.println("Number invalid");
+		}
+
+	}
+
+	// recursive function for binary search
+	int binary(int[] arr, int num, int st, int end) {
+		int mid;
+		while (st <= end) {
+			mid = (st + end) / 2;
+			if (arr[mid] == num) {
+				return mid;
+			} else if (num > arr[mid]) {
+				return (binary(arr, num, mid + 1, end));
+			} else {
+				return (binary(arr, num, st, mid - 1));
+			}
+		}
+		return -1;
+	}
+}
