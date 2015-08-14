00aa6993dc729c2821ab15865d5b7e543c10d531
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-14 12:05:15 +0530

    Update Matrix.java
    source code to transpose the matrix

--- PF/OOPAssignment1/Matrix.java
+++ PF/OOPAssignment1/Matrix.java
@@ -1,18 +1,37 @@
 
+/**
+ * @author Sumitra
+ * this class is to get a matrix and to transpose the matrix and to print that.
+ */
 public class Matrix {
 	 int data[][];
 	 int noRows;
 	 int noCols;
+	 /**
+	  * 
+	  * @param row number of row for matrix
+	  * @param col number of column for matrix
+	  */
 	 public Matrix(int row,int col){
 	 this.noRows = row;
 	 this.noCols = col;
 	  data = new int[row][col];
 	 }
+	 /**
+	  * 
+	  * @param row number of row in which value is inserted
+	  * @param col number of column in which value is to be inserted
+	  * @param val the value top be set at index [row][col]
+	  */
 	 public void addElements(int row, int col, int val ) {
 		 
 	 data[row][col]=val;
 		 
 	 }
+	 /**
+	  * 
+	  *  @return will return the matrix after transpose
+	  */
 	 public Matrix transpose(){
 	 // matrix Transpose logic
 		 Matrix mat=new Matrix(noCols,noRows);
@@ -27,6 +46,9 @@ public class Matrix {
 		 return mat;
 		 
 	 }
+	 /**
+	  * this method is to print the whole matrix.
+	  */
 	 public void show() {//logic to display}
 		 for(int i=0;i<noRows;i++)
 		 {
