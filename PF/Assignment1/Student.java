983150b14b30795aadc928e900b4490fd9185c0b
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-14 18:37:24 +0530

    Create Student.java

---
+++ PF/Assignment1/Student.java
@@ -0,0 +1,31 @@
+public class Student extends Person 
+{
+	private int studentId;
+	private String[] courses;
+	
+	public Student(int uid, String name, int studentId)
+	{
+		super(uid,name);
+		this.studentId = studentId;
+		
+	}
+	public int getStudentId()
+	{ 	return studentId;
+	}
+	public void setStudentId(int studentId) 
+	{ 
+		this.studentId = studentId;
+	}
+	public String[] getCourses()
+	{ 	
+		return courses;
+	}
+	public void setCourses(String[] courses) 
+	{ 
+			this.courses = courses;
+	}
+	public String toString()
+	{
+		return super.toString()+ ", StudentId "+studentId;
+	}
+}
