4c230be7506b605f08753e7d939acba9dac88d1d
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-14 15:59:57 +0530

    Update TestForPermuation.java

--- PF/Assignment4/TestForPermuation.java
+++ PF/Assignment4/TestForPermuation.java
@@ -8,10 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class TestForPermuation {
 
-	Permutation objectOfPermutation=new Permutation();
+	Permutation objectOfPermutation = new Permutation();
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -30,37 +30,39 @@ public class TestForPermuation {
 
 	@Test
 	public void testForRepetedString() {
-		objectOfPermutation.result=new ArrayList<String>();
-		objectOfPermutation.result=objectOfPermutation.permutation("AAB");
-		String expected[]={"ABA","AAB","BAA"};
-		int i=0;
-		for(String a:objectOfPermutation.result)
-		{
-			assertEquals(expected[i],a);
+		Permutation.result = new ArrayList<String>();
+		Permutation.result = objectOfPermutation.permutation("AAB");
+		String expected[] = { "ABA", "AAB", "BAA" };
+		int i = 0;
+		for (String a : Permutation.result) {
+			assertEquals(expected[i], a);
 			i++;
 		}
 	}
+
 	@Test
 	public void testForStringOfThree() {
-		objectOfPermutation.result=new ArrayList<String>();
-		objectOfPermutation.result=objectOfPermutation.permutation("ABC");
-		String expected[]={"ACB","BCA","ABC","CBA","BAC","CAB"};
-		int i=0;
-		for(String a:objectOfPermutation.result)
-		{
-			assertEquals(expected[i],a);
+		Permutation.result = new ArrayList<String>();
+		Permutation.result = objectOfPermutation.permutation("ABC");
+		String expected[] = { "ACB", "BCA", "ABC", "CBA", "BAC", "CAB" };
+		int i = 0;
+		for (String a : Permutation.result) {
+			assertEquals(expected[i], a);
 			i++;
 		}
 	}
+
 	@Test
 	public void testForStringOfFour() {
-		objectOfPermutation.result=new ArrayList<String>();
-		objectOfPermutation.result=objectOfPermutation.permutation("ABCD");
-		String expected[]={"BADC","BCDA","DABC","DCBA","CADB","CBDA","DACB","DBCA","ACBD","ADBC","BCAD","BDAC","ABCD","ADCB","CBAD","CDAB","ABDC","ACDB","BACD","BDCA","CABD","CDBA","DBAC","DCAB"};
-		int i=0;
-		for(String a:objectOfPermutation.result)
-		{
-			assertEquals(expected[i],a);
+		Permutation.result = new ArrayList<String>();
+		Permutation.result = objectOfPermutation.permutation("ABCD");
+		String expected[] = { "BADC", "BCDA", "DABC", "DCBA", "CADB", "CBDA",
+				"DACB", "DBCA", "ACBD", "ADBC", "BCAD", "BDAC", "ABCD", "ADCB",
+				"CBAD", "CDAB", "ABDC", "ACDB", "BACD", "BDCA", "CABD", "CDBA",
+				"DBAC", "DCAB" };
+		int i = 0;
+		for (String a : Permutation.result) {
+			assertEquals(expected[i], a);
 			i++;
 		}
 	}
