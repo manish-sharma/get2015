c3abac2c5bf48faec94cba3b48aa1ea3c9229579
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-14 16:54:21 +0530

    Create CarServiceAppointmentSystem.java

---
+++ PF/Assignment1/CarServiceAppointmentSystem.java
@@ -0,0 +1,30 @@
+/**
+ * Program to manage the services of car to the mechanics
+ * there are three types of car Suv Sedan Hatchback and their rate is 500,300,100 respectively 
+ *one mechanic is specialist of one of the three cars and it can service atmost one car 
+ * */
+package CarServiceAppointmentSystem;
+import java.util.HashMap;
+public class CarServiceAppointmentSystem
+{
+
+	public static void main(String[] args)
+	{
+		ServiceStation serviceStation = new ServiceStation();
+		serviceStation.readMechanics();
+		serviceStation.readCars();
+		serviceStation.doAllotment();
+		HashMap<String, Integer> carProcessingList = serviceStation.getCarProcessingStatus();
+
+		System.out.println("CarType \t Number");
+
+		for (String carType : carProcessingList.keySet())
+		{
+			System.out.println(carType + "\t\t " + carProcessingList.get(carType));
+		}
+
+		System.out.println("Total Earning \t " + serviceStation.getEarning());
+
+	}
+
+}
