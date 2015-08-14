f1e5a9db1d719a03b4d07a930781174d5d50943e
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:52:07 +0530

    Create Circle.java

---
+++ PF/Assignment1/Circle.java
@@ -0,0 +1,22 @@
+
+//program to calculate area of circle
+public class Circle implements Shape {
+
+	private int radius;
+
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	public void draw() {
+		// Circle drawing logic
+		System.out.println("this is circle");
+	}
+
+	public double getArea() {
+		// Circle area calculation
+		double area = 3.14 * radius * radius;
+		return area;
+	}
+
+}
