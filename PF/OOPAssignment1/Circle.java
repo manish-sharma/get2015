8cf85b4103e2d0a625d4f3d4d34b9df88448fbe9
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-14 11:58:03 +0530

    Update Circle.java
    this is to calculate area of circle source code

--- PF/OOPAssignment1/Circle.java
+++ PF/OOPAssignment1/Circle.java
@@ -1,11 +1,28 @@
 
+/**
+ * @author Sumitra
+ *this class is implemented frorm interface shape which is having two functions void draw() and void getArea().
+ */
 public class Circle implements Shape {
 	private int radius;
-	public Circle(int radius){this.radius = radius;}
+	/**
+	 * 
+	 * @param radius is the value of radius of circle.
+	 */
+	public Circle(int radius)
+	{
+		this.radius = radius;
+	}
+	/**
+	 * this method is to print a suitable message
+	 */
 	public void draw(){
 	// Circle drawing logic
 		System.out.println("this is circle");
 	}
+	/**
+	 * this method is to calcutae the area of circle and than it will return.
+	 */
 	public double getArea(){
 	// Circle area calculation
 		double area=3.14*radius*radius;
@@ -14,3 +31,4 @@ public class Circle implements Shape {
 
 
 }
+//end of class
