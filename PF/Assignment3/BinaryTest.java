2b1d2808227c917c9ef3293328eac5b40676bedf
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:41:08 +0530

    Update and rename Prg2bTest.java to BinaryTest.java

--- PF/Assignment3/BinaryTest.java
+++ PF/Assignment3/BinaryTest.java
@@ -8,9 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Prg2bTest {
-	Prg2b obj=new Prg2b();
-	int[] arr= {2,5,8,9,10, 77, 55};
+public class BinaryTest {
+	BinarySearch obj = new BinarySearch();
+	int[] arr = { 2, 5, 8, 9, 10, 77, 55 };
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -29,13 +30,12 @@ public class Prg2bTest {
 
 	@Test
 	public void test() {
-		
-		assertEquals("nuber not found",-1,obj.binary(arr,88,0,6));  // test for(88)
-		assertEquals("nuber found",5,obj.binary(arr,77,0,6));  // test for(77)
-		
-	}
-	
+
+		assertEquals("nuber not found", -1, obj.binary(arr, 88, 0, 6)); // test
+																		// for(88)
+		assertEquals("nuber found", 5, obj.binary(arr, 77, 0, 6)); // test
+																	// for(77)
 
 	}
 
-
+}
