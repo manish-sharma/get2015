efc3cf03a8211e43d019832097210810ad2b2fe6
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-14 16:56:48 +0530

    Create Car.java

---
+++ PF/Assignment1/Car.java
@@ -0,0 +1,13 @@
+package CarServiceAppointmentSystem;
+public class Car
+{
+	String carNumber;
+	String carType;
+
+	public Car(String CarNum, String CarTyp)
+	{
+		carNumber = CarNum;
+		carType = CarTyp;
+	}
+}
+
