3d0be4fc20e3c941aa92070a2a525f12f6f16b6e
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-14 15:32:13 +0530

    Update and rename Prg1bTest.java to GreatestCommonTest.java

--- PF/Assignment3/GreatestCommonTest.java
+++ PF/Assignment3/GreatestCommonTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Prg1bTest {
+public class GreatestCommonTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,7 +28,7 @@ public class Prg1bTest {
 
 	@Test
 	public void test() {
-		 Prg1b obj=new Prg1b();
+		 GreatestCommon obj=new GreatestCommon();
 			assertEquals("GCD calculated",1,obj.gcd(2, 1));  // test for(2,1)
 			assertEquals("GCD calculated",6,obj.gcd(12, 18));  // test for(12,18)
 			assertEquals("GCD calculated",1,obj.gcd(100, 3));  // test for(100,3)
