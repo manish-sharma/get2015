471e1676f5a086dd848fb1bd29ee4490a24bf383
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-14 12:26:44 +0530

    Update Square.java
    source code for square class

--- PF/OOPAssignment1/Square.java
+++ PF/OOPAssignment1/Square.java
@@ -1,17 +1,36 @@
 
-public class Square implements Shape {
+/**
+ * @author Sumitra
+ * this class is created to calculate the area of square and in implemented from shape
+ */
+public class Square implements Shape 
+{
 	private int side;
-	public Square(int side){this.side = side;}
-	public void draw(){
+	/**
+	 * 
+	 * @param side is the length of one side of square
+	 */
+	public Square(int side)
+	{
+		this.side = side;
+	}
+	/**
+	 * method is created to print a appropriate message about the class
+	 */
+	public void draw()
+	{
 		// Square drawing logic
 		System.out.println("this is Square");
-		}
-	
-	public double getArea(){
+	}
+	/**
+	 * will return the area of square
+	 */
+	public double getArea()
+	{
 		// Square area calculation
 		double area=side*side;
 		return area;
-		}
 	}
+}
 
 
