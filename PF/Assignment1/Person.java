352c8f1f6d6c3ff36e800187630e550176b79d01
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-14 18:34:01 +0530

    Create Person.java

---
+++ PF/Assignment1/Person.java
@@ -0,0 +1,30 @@
+public class Person 
+{
+	private int uid;
+	private String name;
+	public Person(int uid, String name)
+	{
+		this.uid = uid;
+		this.name = name;
+	}
+	public int getUid()
+	{
+		return uid;
+	}
+	public void setUid(int uid)
+	{
+		this.uid = uid;
+	}
+	public String getName()
+	{ 
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String toString()
+	{
+		return "Name: "+name+", uid "+uid;
+	}
+}
