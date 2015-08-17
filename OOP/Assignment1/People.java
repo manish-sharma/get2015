0b92c417b46a44813abf54f2bb61afada7a6f10d
meta-khemanshu-rao <khemanshu.rao@metacube.com>
2015-08-17 10:10:17 +0530

    submission of Assignment1

--- OOP/Assignment1/People.java
+++
@@ -1,6 +0,0 @@
-package oop1;
-
-public abstract class People {
-	String ID;
-	String name;
-}
--- OOP/Assignment1/Shape.java
+++
@@ -1,7 +0,0 @@
-package oop1;
-
-public interface Shape {     
-    void draw();
-    double getArea();
-}
-
