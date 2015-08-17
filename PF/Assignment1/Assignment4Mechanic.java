fbe630043ffbd30d92583cc7567b011cdf6377e5
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-17 16:50:09 +0530

    Update and rename Mechanic.java to Assignment4Mechanic.java

--- PF/Assignment1/Assignment4Mechanic.java
+++ PF/Assignment1/Assignment4Mechanic.java
@@ -1,12 +1,12 @@
-package CarServiceAppointmentSystem;
-public class Mechanic extends People
+
+public class Assignment4Mechanic extends Assignment4People
 {
 
 	String type;
 	String rate;
 	boolean isAvailable;
 
-	public Mechanic(String mechId, String mechName, String mechType, String mechRate)
+	public Assignment4Mechanic(String mechId, String mechName, String mechType, String mechRate)
 	{
 		mechID = mechId;
 		name = mechName;
