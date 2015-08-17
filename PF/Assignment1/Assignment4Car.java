e1457fbbb499ab69bcbd1e96fec1d510f1beca91
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-17 16:45:36 +0530

    Update and rename Car.java to Assignment4Car.java

---
+++ PF/Assignment1/Assignment4Car.java
@@ -0,0 +1,13 @@
+
+public class Assignment4Car
+{
+	String carNumber;
+	String carType;
+
+	public Assignment4Car(String CarNum, String CarTyp)
+	{
+		carNumber = CarNum;
+		carType = CarTyp;
+	}
+}
+
