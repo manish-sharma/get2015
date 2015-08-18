c5c7d6501bf494702e535f979d501a2a096212bc
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-18 17:24:56 +0530

    Create UserInformation.java

---
+++ PF/OOPAssignment3/UserInformation.java
@@ -0,0 +1,42 @@
+
+/**
+ * @author Sumitra
+ *this is for user input and all getter are there.
+ */
+public class UserInformation {
+	String from;
+	String to;
+	String type;
+	int wantedSeat;
+	String name;
+	public UserInformation(String from, String to, String type, int wantedSeat,
+			String name) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.type = type;
+		this.wantedSeat = wantedSeat;
+		this.name = name;
+	}
+	public String getFrom() {
+		return from;
+	}
+	
+	public String getTo() {
+		return to;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public int getWantedSeat() {
+		return wantedSeat;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+
+}
