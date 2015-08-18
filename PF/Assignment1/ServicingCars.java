905bfb5c048042e2bf31e81997764ea894e5af1a
meta-anshita-arya <anshita.arya@metacube.com>
2015-08-18 14:49:23 +0530

    Update ServicingCars.java

--- PF/Assignment1/ServicingCars.java
+++ PF/Assignment1/ServicingCars.java
@@ -1,26 +1,26 @@
 public class ServicingCars {
-	String carType;// cartype of cars entering service station
-	int carNumber;// car number
+	String carType;
+	int carNumber;
 
-	public ServicingCars(String carType, int carNumber) {
+	public ServicingCars(String carType, int carNumber) { // constructor calling
 		super();
 		this.carType = carType;
 		this.carNumber = carNumber;
 	}
 
-	public int getCarNumber() {
+	public int getCarNumber() { // getting car id
 		return carNumber;
 	}
 
-	public void setCarNumber(int carNumber) {
+	public void setCarNumber(int carNumber) { // setting car id
 		this.carNumber = carNumber;
 	}
 
-	public String getCarType() {
+	public String getCarType() { // getting car type
 		return carType;
 	}
 
-	public void setCarType(String carType) {
+	public void setCarType(String carType) { // setting car type
 		this.carType = carType;
 	}
 }
