3383bb65ec8b97fbd4501cc006e1fe89dabdced2
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-19 17:13:21 +0530

    Create PassengerTrain.java

---
+++ PF/OOPAssignment3/RefactoredCode/PassengerTrain.java
@@ -0,0 +1,13 @@
+/**
+ * @author Sumitra
+ *this class is for passenger type and extends the abstract class train
+ */
+public class PassengerTrain extends Train {
+
+	public PassengerTrain(int trainNumber, String source, String destination,
+			String type, int available, double price, int time) {
+		super(trainNumber, source, destination, type, available, price, time);
+		// TODO Auto-generated constructor stub
+	}
+
+}
