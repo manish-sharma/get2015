b4875cd567c06639d925eea84f16ace438c32854
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-19 17:17:36 +0530

    Create TimeComparatorForGoods.java

---
+++ PF/OOPAssignment3/RefactoredCode/TimeComparatorForGoods.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+/**
+ * @author Sumitra
+ *this class is to sort the list of goods train and implements the Comparator interface
+ */
+public class TimeComparatorForGoods implements Comparator {
+	public int compare(Object o1, Object o2) {
+		GoodsTrain p1 = (GoodsTrain) o1;
+		GoodsTrain p2 = (GoodsTrain) o2;
+		if (p1.getTime() == 0) {
+			return 0;
+		} else if (p1.getTime() > p2.getTime()) {
+			return 1;
+		} else
+			return -1;
+
+	}
+}
