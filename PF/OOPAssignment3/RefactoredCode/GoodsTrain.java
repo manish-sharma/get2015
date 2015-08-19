9aed3553051a9dc8d4d3011e6bd17dbdae68281e
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-19 17:11:47 +0530

    Create GoodsTrain.java

---
+++ PF/OOPAssignment3/RefactoredCode/GoodsTrain.java
@@ -0,0 +1,13 @@
+/**
+ * @author Sumitra
+ *this class is for goods train and it extends the train class
+ */
+public class GoodsTrain extends Train {
+
+	public GoodsTrain(int trainNumber, String source, String destination,
+			String type, int available, double price, int time) {
+		super(trainNumber, source, destination, type, available, price, time);
+		// TODO Auto-generated constructor stub
+	}
+
+}
