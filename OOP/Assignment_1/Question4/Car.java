3584f33568f6916d85b8f466ca507b55ffe1125c
meta-neha-bansal <neha.bansal@metacube.com>
2015-08-19 11:48:08 +0530

    oop assignment 1

---
+++ OOP/Assignment_1/Question4/Car.java
@@ -0,0 +1,35 @@
+/* This is Car class which is used to store number and type of car as well as retrieve them.
+ */
+package Question4;
+
+public class Car 
+{
+	int carNumber;
+	String carType;
+	/**
+	 * @return the carNumber
+	 */
+	public int getCarNumber() {
+		return carNumber;
+	}
+	/**
+	 * @param carNumber the carNumber to set
+	 */
+	public void setCarNumber(int carNumber) {
+		this.carNumber = carNumber;
+	}
+	/**
+	 * @return the carType
+	 */
+	public String getCarType() {
+		return carType;
+	}
+	/**
+	 * @param carType the carType to set
+	 */
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+	
+
+}
