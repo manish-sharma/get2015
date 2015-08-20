e21cbc1623bb143db2b912cc36713eab6845d76a
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-20 14:28:44 +0530

    Update UserInput.java

--- PF/OOPAssignment3/RefactoredCode/UserInput.java
+++ PF/OOPAssignment3/RefactoredCode/UserInput.java
@@ -18,16 +18,38 @@ public class UserInput {
 			menu.displayMainMenu();
 			int type = scanner.nextInt();
 			DisplayTrainTimeTable display = new DisplayTrainTimeTable();
-			display.display(type);
+			if(type==1)
+			{
+			display.displayPassenger();
+			}
+			else
+				if(type==2)
+				{
+					display.displayGoods();
+				}
+				else
+				{
+					System.out.println("entered wrong choice");
+				}
 			System.out.println("Enter Source");
 			String source = scanner.next();
 			System.out.println("Enter Destination");
 			String destination = scanner.next();
 			int wanted = 0;
+			
+			
 			UserInformation user = new UserInformation(source, destination,
 					wanted, name);
-			display.requiredTrain(type, user.getFrom(), user.getTo(), wanted,
+			if(type==1)
+			{
+			display.requiredPassengerTrain(user.getFrom(), user.getTo(), wanted,
 					user);
+			}
+			else
+				if(type==2){
+					display.requiredGoodsTrain(user.getFrom(), user.getTo(), wanted,
+							user);
+				}
 			menu.continuechoice();
 			contiunebook = scanner.next().charAt(0);
 		} while (contiunebook == 'Y');
