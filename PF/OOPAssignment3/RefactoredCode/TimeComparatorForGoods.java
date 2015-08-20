e4d1d2c8b65618286344be76be9a7d4090cb7366
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-20 14:28:02 +0530

    Update TimeComparatorForGoods.java

--- PF/OOPAssignment3/RefactoredCode/TimeComparatorForGoods.java
+++ PF/OOPAssignment3/RefactoredCode/TimeComparatorForGoods.java
@@ -9,7 +9,7 @@ public class TimeComparatorForGoods implements Comparator {
 	public int compare(Object o1, Object o2) {
 		GoodsTrain p1 = (GoodsTrain) o1;
 		GoodsTrain p2 = (GoodsTrain) o2;
-		if (p1.getTime() == 0) {
+		if (p1.getTime() == p2.getTime()) {
 			return 0;
 		} else if (p1.getTime() > p2.getTime()) {
 			return 1;
