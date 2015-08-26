ea83baa979cca793a02f9bce78cfc26c5f5eb8db
banwari kevat <banwari.kevat@metacube.com>
2015-08-26 10:29:44 +0530

    Added All Assignment

--- DS/DS-seesion-1/SinglyLinkedList.java
+++ DS/DS-seesion-1/SinglyLinkedList.java
@@ -15,6 +15,7 @@
 		this.next = null;
 	}
 }
+ // This class contains the Linked list operation
 public class SinglyLinkedList {
 		private Node first;
 		
@@ -22,12 +23,15 @@ public class SinglyLinkedList {
 			this.first=null;
 		}
 		
-	
+	//This method create node and then return refrence of that node
 	public Node createNode(int item) {
 		 Node node = new Node(item);
 		 return node;
 	}
-	
+	/*
+	 * This method insert the node at beginning
+	 * @param item is the which will be inserted into linked list
+	 */
 	public void insertAtBegin( int item ) {
 		if(first==null) {
 			first = createNode(item);
@@ -39,6 +43,7 @@ public class SinglyLinkedList {
 		}
 	}
 	
+	//This method insert the node at last
 	public void insertAtLast(int item) {
 		if(first==null) {
 			first = createNode(item);
@@ -49,20 +54,74 @@ public class SinglyLinkedList {
 				traverse=traverse.next;
 				Node node = createNode(item);
 				traverse.next=node;			
-		}
-		      
+		}	      
 	}
 	
+	//This method remove the item from list 
 	public boolean removeItem( int item) {
 		if(first==null) {
 			return false;
 		}
 		else {
-			
+			Node traverse = first;
+			Node temp =null;
+			while( traverse.next != null) {
+				if(traverse.item != item) {
+				     temp=traverse;
+				     traverse = traverse.next;
+				} 
+				else {
+					 temp.next=traverse.next;
+					 traverse=traverse.next;
+				}	 
+			}
+			if(traverse.next!=null){
+			    temp.next=traverse.next;
+			    return true;
+			} 
+			else return false;
 		}
 		
 	}
 	
+	//this method will reverse the list
+	public void reverseList() {
+		Node tempnode1,tempnode2,traverse;
+		traverse=first;
+		tempnode1=null;
+		tempnode2=traverse.next;
+		while(traverse.next!=null) {
+			traverse.next=tempnode1;
+			tempnode1=traverse;
+			traverse=tempnode2;
+			tempnode2=tempnode2.next;
+		}
+		traverse.next=tempnode1;
+		first=traverse;
+	}
+	
+	
+	//This method sort the element of list
+	public void sortList() {
+		Node tempnode1 = first, tempnode2;
+		int temp;
+		 // apply simple sorting to sort the elements of Linked list
+		while( tempnode1 != null ) {
+			tempnode2 = first;
+			while(tempnode2.next != null) {
+				if( tempnode2.item > tempnode2.next.item ){
+					temp = tempnode2.item;
+					tempnode2.item = tempnode2.next.item;
+					tempnode2.next.item = temp;
+				}
+				tempnode2 = tempnode2.next;
+			}
+			tempnode1 = tempnode1.next;
+		}
+	}
+	
+	
+	//This method will display the list
 	public void showList(){
 	   Node traverse = first;
 	   while(traverse!=null) {
@@ -76,15 +135,20 @@ public static void main(String []  arg) {
 	
 	SinglyLinkedList obj = new SinglyLinkedList();
 	obj.insertAtBegin(78);
-	obj.insertAtBegin(77);
-	obj.insertAtBegin(13);
+	obj.insertAtBegin(18);
+	obj.insertAtBegin(19);
 	obj.insertAtLast(30);
+	obj.insertAtLast(15);
+	obj.insertAtBegin(88);
+	obj.insertAtBegin(13);
 	
 	obj.showList();
-		
+	System.out.println();
+	obj.sortList();
+	obj.showList();
+	
 }
 	
 	
 
 }
- 
