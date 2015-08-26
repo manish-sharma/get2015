ddfcce8c92280148de0eae200ba8f9c385cfad00
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-26 17:00:42 +0530

    Update BinarySearch.java

--- DS/Assignment1/Optional/BinarySearch.java
+++ DS/Assignment1/Optional/BinarySearch.java
@@ -13,23 +13,23 @@ public class BinarySearch {
 		
 	}
 	
-	int searchElement(int array[], int value1) {
+	int searchElement(int array[], int value) {
 		int lower, upper, mid;
 		lower = 0;
 		upper = array.length - 1;
 		mid = (lower + upper) / 2 ;
-		for(int value : array)
-			System.out.println(value);
-		while(upper > lower) {
+		while(upper >= lower) {
 			mid = (lower + upper) / 2 ;
-			if (array[mid] == value1) {
+			if (array[mid] == value) {
 				break;
-			} else if (array[mid] < value1) {
+			} else if (array[mid] < value) {
 				lower = mid + 1;
-			} else if(array[mid] > value1) {
+			} else if(array[mid] > value) {
 				upper = mid - 1;
 			}
 		}
+		if(array[mid] != value)
+			mid = -1;
 		return mid;
 	}
 }
