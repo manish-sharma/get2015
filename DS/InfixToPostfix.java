e15d7e70fda9b941a30493864b4211c2c23565bc
ravikajain <ravika.jain@metacube.com>
2015-08-26 19:28:04 +0530

    Create InfixToPostfix.java

---
+++ DS/InfixToPostfix.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+
+public class InfixToPostfix {
+
+	public InfixToPostfix() {
+		// TODO Auto-generated constructor stub
+	} 
+	/**
+	 * purpose : to get the priority of operator
+	 * @param operator = its priority will be checked
+	 * @return priority
+	 */
+	
+	private int priority(char operator) {
+		switch(operator) {
+		case '(':
+			return 0;
+		case '+':
+		case '-':
+			return 1;
+		case '*':
+		case '/':
+			return 3;
+		}
+		return -1;
+	}
+	/**
+	 * purpose : to check that current character is operand or not
+	 * @param c = current character
+	 * @return true if operand,otherwise false
+	 */
+	private boolean isOperand(char c) {
+		if(c >='0' && c <= '9' ) {
+			return true;
+		} else if(c >= 'a' && c <= 'z') {
+			return true;
+		} else if(c >= 'A' && c <= 'Z') {
+			return true;
+		}
+		return false;		
+	}
+	/**
+	 * purpose: to check current character is operator or not
+	 * @param c = current character
+	 * @return true if operand,otherwise false
+	 */
+	private boolean isOperator(char c) {
+		if(c == '/' || c == '*' || c == '+' || c == '-' || c== '^' ) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * purpose: to convert infix into post fix
+	 * @param infix = input String
+	 */
+	public String infixToPostfix (String infix) {
+		String postFix ="";
+		Stack stack = new Stack();
+		stack.push('(');
+		for(int count = 0; count < infix.length()+1; count++) {
+			char currentChar ;
+			if(count == infix.length()) {
+				currentChar = ')';
+			} else {
+				currentChar = infix.charAt(count);
+			}
+			
+			if(isOperand(currentChar)) {
+				postFix += currentChar;
+				System.out.print("" + currentChar);
+			} else if(isOperator(currentChar)) {
+				if(priority(currentChar) < priority((char)stack.top.data)) {
+					postFix +=(char)stack.top.data;
+					System.out.print("" + (char)stack.top.data);
+				 stack.pop();	
+				} else {
+					stack.push(currentChar);
+				}
+			} else if(currentChar == ')') {								//if current character is ')' than we have to pop all the elements of stack till ')'
+				while(stack.top.data != '(') {
+					postFix +=(char)stack.top.data;
+					System.out.print("" + (char)stack.top.data);
+					stack.pop();
+				}
+				stack.pop();
+			} else if(currentChar == '(') {
+				stack.push(currentChar);
+			} else {
+				System.out.println("invalid infix");
+			}
+		}
+		return postFix;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("enter the infix String");
+		Scanner scanner = new Scanner(System.in);
+		String infix = scanner.next();
+		InfixToPostfix infixToPostfix = new InfixToPostfix();
+		infixToPostfix.infixToPostfix(infix);
+		scanner.close();
+		
+	}
+}
