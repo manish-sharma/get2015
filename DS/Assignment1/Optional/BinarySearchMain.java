581d41d9458cea45d2906f8aa6608eedc697a23b
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-26 17:00:26 +0530

    Update BinarySearchMain.java

--- DS/Assignment1/Optional/BinarySearchMain.java
+++ DS/Assignment1/Optional/BinarySearchMain.java
@@ -8,8 +8,13 @@ public class BinarySearchMain {
 		BinarySearch binarySearchObject = new BinarySearch(10);
 		int array[] = {10,20,30,40,50,60,70,80,90,100};
 		binarySearchObject.populateArray(array);
+		System.out.print("Enter Number to be searched: ");
 		int value = new Scanner(System.in).nextInt();
-		System.out.println(binarySearchObject.searchElement(array, value));
+		int index = binarySearchObject.searchElement(array, value);
+		if(index == -1)
+			System.out.println("Number doesn't exist!!");
+		else
+			System.out.println( index + 1);
 	}
 
 }
