07951e537b0a1175f80a0e8a7457351fd69c0375
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-27 18:55:47 +0530

    Create Node.java

---
+++ DS/Assignment3/Node.java
@@ -0,0 +1,37 @@
+package preordertraversal;
+
+public class Node {
+
+	private Node leftNode;
+	private int dataNode;
+	private Node rightNode;
+	
+	public Node() {
+		leftNode = null;
+		rightNode = null;
+	}
+	public Node getLeftNode() {
+		return leftNode;
+	}
+	
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+	
+	public int getDataNode() {
+		return dataNode;
+	}
+	
+	public void setDataNode(int dataNode) {
+		this.dataNode = dataNode;
+	}
+	
+	public Node getRightNode() {
+		return rightNode;
+	}
+	
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+	
+}
