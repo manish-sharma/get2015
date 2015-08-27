31fdfd46651b311b71ddeb5b38806f1c46aa1fc4
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-27 19:02:37 +0530

    Update PostOrderTraverse.java

--- DS/Assignment3/PostOrderTraverse.java
+++ DS/Assignment3/PostOrderTraverse.java
@@ -1,7 +1,14 @@
+/**
+ * @author Girdhari
+ * This class will perform Post Order Traversal on a tree
+ */
+
 package preordertraversal;
 
+/* Start of PostOrderTraverse class*/
 public class PostOrderTraverse {
 	private static String postOrderString = "";
+	/* Start of postOrderTraversal function */
 	public Node postOrderTraversal(Node root) {
 		if( root != null ) {
 			postOrderTraversal(root.getLeftNode());
@@ -11,8 +18,11 @@ public class PostOrderTraverse {
 		}
 		return null;
 	}
+	/* End of postOrderTraversal function */
+	//getter of postOrderString variable
 	public static String getPostOrderString() {
 		return postOrderString;
 	}
 
 }
+/* End of PostOrderTraverse class*/
