830a959f849af97e14cfcc9e1ab391cf2d8797b0
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-27 11:09:40 +0530

    Update SocialNetworkMenu.java

--- OOP/Assignment5/SocialNetworkMenu.java
+++ OOP/Assignment5/SocialNetworkMenu.java
@@ -1,17 +1,16 @@
-package SocialNetwork;
+
 public class SocialNetworkMenu  {
 	
 		public void showMenu()
 		{
 			Menu menu = new Menu();
 			ActionableMenuItem item1 = new ActionableMenuItem("Press 1 to view Social Netowrk");
-			ActionableMenuItem item2 = new ActionableMenuItem("Press 2 to getRecords of all nodes");
-			ActionableMenuItem item3 = new ActionableMenuItem("Press 3 to add a new node");
-			ActionableMenuItem item4 = new ActionableMenuItem("Press 4 to add an edge");
-			ActionableMenuItem item5 = new ActionableMenuItem("Press 5 to remove a node");
-			ActionableMenuItem item6 = new ActionableMenuItem("Press 6 to search an entity by name");
-			ActionableMenuItem item7 = new ActionableMenuItem("Press 7 to remove an edge netween two nodes");
-			ActionableMenuItem item8 = new ActionableMenuItem("Press 8 to exit");
+			ActionableMenuItem item2 = new ActionableMenuItem("Press 2 to add a new Person");
+			ActionableMenuItem item3 = new ActionableMenuItem("Press 3 to add an new friend");
+			ActionableMenuItem item4 = new ActionableMenuItem("Press 4 to remove a Person");
+			ActionableMenuItem item5 = new ActionableMenuItem("Press 5 to search an Person by name");
+			ActionableMenuItem item6 = new ActionableMenuItem("Press 6 to remove a Friend");
+			ActionableMenuItem item7 = new ActionableMenuItem("Press 7 to exit");
 			
 			menu.addMenuItem(item1);
 			menu.addMenuItem(item2);
@@ -20,7 +19,6 @@ public class SocialNetworkMenu  {
 			menu.addMenuItem(item5);
 			menu.addMenuItem(item6);
 			menu.addMenuItem(item7);
-			menu.addMenuItem(item8);
 			menu.display();
 		}
 
