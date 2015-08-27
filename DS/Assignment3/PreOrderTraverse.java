9aebb0a1d72947356eebda1999f4840b707e766c
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-27 19:00:44 +0530

    Update PreOrderTraverse.java

--- DS/Assignment3/PreOrderTraverse.java
+++ DS/Assignment3/PreOrderTraverse.java
@@ -1,7 +1,13 @@
-package preordertraversal;
+/**
+ * @author Girdhari
+ * This class will perform PreOrderTraversal of a tree
+ * */
 
+package traversal;
+/* Start of PreOrderTraverse class */
 public class PreOrderTraverse {
 	private static String preOrderString = "";
+	/* Start of preOrderTraversal function */
 	Node preOrderTraversal(Node root) {
 		if( root != null ) {
 			System.out.print(root.getDataNode());
@@ -11,8 +17,10 @@ public class PreOrderTraverse {
 		}
 		return null;
 	}
+	/* End of preOrderTraversal function */
+	//getter of preOrderString variable	
 	public static String getPreOrderString() {
 		return preOrderString;
 	}
-
 }
+/* End of PreOrderTraverse class */
