d9114b84b617f9eb9bf605b50e8b58613fba1143
banwari kevat <banwari.kevat@metacube.com>
2015-08-28 10:21:06 +0530

    Added DS Session 2 Assignments

--- DS/DS-Session-2/Problem2/CollegeCounseling.java
+++ DS/DS-Session-2/Problem2/CollegeCounseling.java
@@ -13,19 +13,22 @@ public class CollegeCounseling  {
 	
 	final String CANDIDATE_FILE="C://Users/Banwari/workspace/DS-Session-2/src/Candidates.txt";
 	final String COLLEGE_FILE="C://Users/Banwari/workspace/DS-Session-2/src/Colleges.txt";
-	//Create a candidate queue
+	//This method create a candidate queue and return queue
 	public Queue readCandidatesListSortedByRank()
 	{
 		Queue candidateQueue = new Queue();
 		try
-		{
+		{   //Reading of candidates file
 			FileReader fileReader = new FileReader(CANDIDATE_FILE);
 			BufferedReader br = new BufferedReader(fileReader);
+			//This line contains information of students 
 			String currentLine;
 			while( (currentLine = br.readLine()) != null )
-			{   
+			{   //splits the line and get tokens of information
 			    String [] tokens = currentLine.split("\\s+");
-		         Candidate candidate = new Candidate(tokens[1] , tokens[0], Integer.parseInt(tokens[2]) );
+			    //Create candidate
+		        Candidate candidate = new Candidate(tokens[1] , tokens[0], Integer.parseInt(tokens[2]) );
+		        //Put in queue
 		        candidateQueue.enqueue(candidate);
 			}   
 		}
@@ -37,19 +40,22 @@ public class CollegeCounseling  {
 	}
 	
 	
-	//Create a candidate queue
+	    //This method Create a college list and return reference of list
 		public  List<College> readCollegeList()
 		{    
 			 List<College> collegeList = new ArrayList<College>();
 			try
-			{
+			{   //Reading of colleges file
 				FileReader fileReader = new FileReader(COLLEGE_FILE);
 				BufferedReader br = new BufferedReader(fileReader);
+				//This line contains information about college
 				String currentLine;
 				while( (currentLine = br.readLine()) != null )
-				{   
+				{   //splits the line and get tokens of information
 				    String [] tokens = currentLine.split("\\s+");
+				    //Create college object
 			        College college = new College(tokens[0], Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]) );
+			        //Add object of college to list
 			        collegeList.add(college);
 				}   
 			}
@@ -62,7 +68,7 @@ public class CollegeCounseling  {
 		
 		/*
 		 * This method Display the colleges and corresponding details
-		 * 
+		 * @param collegeList is the list of college
 		 */
 		private void displayCollege(List<College> collegeList) {
 			Iterator<College> iterator= collegeList.iterator();
@@ -70,8 +76,8 @@ public class CollegeCounseling  {
 			while(iterator.hasNext())
 			{
 				College college = iterator.next();
-				System.out.println(college.getName()+"\t\t     "+college.getRank()+"\t\t  "+
-				                             college.getAvialableSeats());				
+				System.out.println(college.getName()+"\t\t     "+college.getRank()+"\t\t  "
+				                   +college.getAvialableSeats());				
 			}
 		}
 		
@@ -84,15 +90,24 @@ public class CollegeCounseling  {
 		{
 			Iterator<College> iterator = collegeList.iterator();
 			College college=null;
+			boolean collegeFound = false;
 			while(iterator.hasNext())
-			{
+			{  //Search college of given rank
 				if(rank == (college=iterator.next()).getRank() )
-				{
-					college.setAvialableSeats(college.getAvialableSeats()-1);
+				{   //if college found then decrease seat by one and then return college reference 
+					if(college.getAvialableSeats()>0)
+					{		
+					    college.setAvialableSeats(college.getAvialableSeats()-1);
+					    collegeFound = true;
+					}
 					break;
 				}
-			}				
-			return college;
+			}	
+			//if college available of this rank then college otherwise return null 
+			if(collegeFound)
+			{ return college;  }
+			else 
+			{	return null;   }
 		}
 		
 		/*
@@ -104,21 +119,31 @@ public class CollegeCounseling  {
 		{   
 			List<CandidateAllotedCollege> candidateAllotedCollegeList = new ArrayList<CandidateAllotedCollege>();
 			System.out.print("\tWELCOME TO COLLEGE COUNSELING\n\n");
+			//Call Candidate until queue is empty
 			while(!candidateQueue.isEmpty()) 
-			{
+			{   //Remove candidate from queue
 				Candidate candidate = (Candidate) candidateQueue.dequeue();
 				
 				System.out.println("\t\tWelcome "+candidate.getName()+"\n");
+				//Display Colleges list and availability of seats
 				displayCollege(collegeList);
-				
-				System.out.print("\nChoose college and then \nEnter rank of that college: ");
+				//choose college according list
+				System.out.print("\nChoose college very carefully because you have only one chance and then \nEnter rank of that college: ");
 				int rankOfSelectedCollege = sc.nextInt();
 				College college = decreaseCollegeSeat(collegeList, rankOfSelectedCollege);
-					
-				CandidateAllotedCollege candidateAllotedCollege  = new CandidateAllotedCollege(candidate,college);
-				candidateAllotedCollegeList.add(candidateAllotedCollege);
-				System.out.println("You have alloted to "+college.getName()+ "\n\n ");
-				System.out.print("Press 1 for Stop Counseling or ANY charecter for next candidate: \n");
+				//if college available
+				if(college != null)
+				{	//Create allotment 
+				    CandidateAllotedCollege candidateAllotedCollege  = new CandidateAllotedCollege(candidate,college);
+				    //add allotment to  list
+				    candidateAllotedCollegeList.add(candidateAllotedCollege);
+				    System.out.println("You have alloted to "+college.getName()+ "\n\n ");
+				}
+				else 
+				{
+					 System.out.println("College or Seats is not available according  your choice\n");
+				}
+				System.out.print("Press 1 for Stop Counseling or\nANY charecter for next candidate: \n");
 				String choice;
 				if(  (choice=sc.next()).equals("1"))
 				{
@@ -138,7 +163,7 @@ public class CollegeCounseling  {
 	     List<CandidateAllotedCollege> candidateAllotedCollegeList= collegeCounseling.doCounseling(candidateQueue, collegeList);
 	     
 	     Iterator<CandidateAllotedCollege> iterator = candidateAllotedCollegeList.iterator();
-	     System.out.println("\n\n\t\t1Counseling RESULT");
+	     System.out.println("\n\n\n\n\n\n\t\tCounseling RESULT");
 	     System.out.println("Candidate Name \t\t  Alloted College");
 	     while(iterator.hasNext()) {
 	    	CandidateAllotedCollege temp=iterator.next();
