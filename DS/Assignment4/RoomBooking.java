57646af7ebb99cfb8d9689a99b3376525a365042
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-08-31 17:44:28 +0530

    Create RoomBooking.java

---
+++ DS/Assignment4/RoomBooking.java
@@ -0,0 +1,15 @@
+package hash;
+
+import java.util.Scanner;
+
+/* Starting of RoomBooking Class */
+public class RoomBooking {
+	/* Starting of main function */
+	public static void main(String[] args) {
+		Hash hashObject = new Hash();
+		hashObject.allotRoom();
+	}
+	/* End of main function */
+
+}
+/* End of RoomBooking Class */
