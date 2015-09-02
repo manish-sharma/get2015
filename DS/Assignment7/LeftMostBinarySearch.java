d9bff9c38e2c830916d33be35c8cebdfcb9d4872
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-09-02 14:53:01 +0530

    Create LeftMostBinarySearch.java

---
+++ DS/Assignment7/LeftMostBinarySearch.java
@@ -0,0 +1,44 @@
+
+public class LeftMostBinarySearch {
+	
+	public int leftMostBinarySearch(int inputArray[] , int searchNumber , int startIndex , int endIndex)
+	{
+			
+			int middleIndex = ( startIndex + endIndex ) / 2;//Find the index of  middle element
+			int tempIndex = middleIndex;
+			
+			if( startIndex >=endIndex )//if number is not found in an array
+			{
+				if(inputArray[startIndex]==searchNumber)
+					middleIndex=tempIndex;
+				else
+					middleIndex=-1;
+				
+				return middleIndex;
+			}
+			
+			if(inputArray [middleIndex] == searchNumber)//if number is found at middleIndex
+			{
+				if(middleIndex==0||middleIndex==inputArray.length-1)	
+					return middleIndex;
+				
+				if(inputArray[middleIndex-1]==searchNumber)
+					return leftMostBinarySearch(inputArray, searchNumber, startIndex, middleIndex-1);
+				
+				else
+					return middleIndex;
+			}
+			
+			if(inputArray [middleIndex] >=searchNumber)
+			{
+				tempIndex=middleIndex;	
+				return leftMostBinarySearch(inputArray, searchNumber, startIndex, middleIndex-1);
+			}
+			
+			else
+				return leftMostBinarySearch(inputArray, searchNumber, middleIndex+1, endIndex);
+		
+			
+	}
+
+}
