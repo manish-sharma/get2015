8a28a6e2781e8649f1d5ca55725326d1a93e0677
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-09-02 10:34:31 +0530

    Update FindOccurrence.java

--- DS/Assignment5/FindOccurrence.java
+++ DS/Assignment5/FindOccurrence.java
@@ -7,27 +7,49 @@ package question1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+/* Starting of FindOccurrence class */
 public class FindOccurrence {
-	HashMap<String, List<String>> hashMapObject = new HashMap<String, List<String>>(); 
+	HashMap<Character, String> hashMapObject = new HashMap<Character, String>(); 
+
+	/* Starting of findOccurrence function */
 	public void findOccurrence(String inputString) {
 		int counter;
-		String key = "";
 		int stringLength = inputString.length();
-		for(counter = 0 ; counter < stringLength ; counter++ ) {
-			key = "" + inputString.charAt(counter);
-			List<String> listMap = new ArrayList<String>(); 
-			if(hashMapObject.containsKey(key))
-				hashMapObject.add(key, String.valueOf(counter));
+		String key = "";
+		//set for unique element
+		Set<Character> uniqueElement = new HashSet<Character>();
+		for(int loopCounter = 0 ; loopCounter < stringLength ; loopCounter++) {		//insert the elements in set
+			uniqueElement.add(inputString.charAt(loopCounter));
 		}
+		// insert element in hashmap without value
+		for(Character character : uniqueElement) {
+			hashMapObject.put(character, "");
+		}
+		String positions = "" ;
+		for(Character character : uniqueElement) {
+			for(int loopCounter = 0 ; loopCounter < stringLength ; loopCounter++) {
+				if(inputString.charAt(loopCounter) == character) {
+					positions += loopCounter + ", ";
+				}
+			}
+			hashMapObject.replace(character, positions);
+			positions = "" ;
+		}
+		
 	}
+	/* Ending of findOccurrence function */
 	
+	/* Starting of printOccurence function */
 	public void printOccurence() {
 		for(Map.Entry mapping:hashMapObject.entrySet()){  
 			   System.out.println(mapping.getKey()+" "+mapping.getValue());  
-			  }  
+		}  
 	}
-
+	/* Ending of printOccurence function */
 }
+/* Ending of FindOccurrence function */
