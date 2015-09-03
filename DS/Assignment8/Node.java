be389bb40cb5b4c3f57f2c79cbe3a42e0cff70b4
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-09-03 15:17:46 +0530

    Create Node.java

---
+++ DS/Assignment8/Node.java
@@ -0,0 +1,44 @@
+/**
+ * @author Girdhari
+ * This class will create Node
+ */
+
+package question1;
+
+/* Start of Node class */
+public class Node {
+
+	private Node leftNode;
+	private int dataNode;
+	private Node rightNode;
+	
+	public Node() {
+		leftNode = null;
+		rightNode = null;
+	}
+	public Node getLeftNode() {
+		return leftNode;
+	}
+	
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+	
+	public int getDataNode() {
+		return dataNode;
+	}
+	
+	public void setDataNode(int dataNode) {
+		this.dataNode = dataNode;
+	}
+	
+	public Node getRightNode() {
+		return rightNode;
+	}
+	
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+	
+}
+/* End of Node class */
