0025990af4831ccc8d837ed875f46e07773e91b8
meta-riddhi-jain <riddhi.jain@metacube.com>
2015-09-04 05:11:14 -0700

    Update BinarySearch.java

--- DS/Assignment7/BinarySearch.java
+++ DS/Assignment7/BinarySearch.java
@@ -5,120 +5,59 @@
  */
 public class BinarySearch {
 
-	//method used to check the left most occurrence
-	public int leftMostOccurrence(int arr[], int lower, int upper, int value) {
-		int result = 0;
-		//returns if at any time lower bound is greater than upper bound
-		if (lower > upper) {
-			return -1;
-		}
-		//if the array is of size one than if the value of the element is similar than return its position
-		if (lower == upper) {
-			if (arr[lower] == value)
-				return lower;
+	//binary search algorithm to search the position of an element
+	int binarySearch(int inputArray[], int left, int right, int key) {
+		int mid;
+		// Loop invariant:
+		// 1 left is always less than or equal to right
+		// 2 If element at mid = key,then return
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+
+			if (inputArray[mid] == key) // first comparison
+				return mid;
+
+			if (inputArray[mid] < key) // second comparison
+				left = mid + 1;
 			else
-				return -1;
-		} 
-		//else if lower bound is smaller than upper bound than perform following operations
-		else if (lower < upper) {
-			//calculated middle element
-			int mid = (lower + upper) / 2;
-			//checks value at middle element
-			if (arr[mid] == value) {
-				result = mid;
-				//if value is equal than it is recursively called for left sub array
-				result = leftMostOccurrence(arr, lower, mid - 1, value);
-				//if at any position value returned by the method is -1 than method finally gives the index of middle element
-				if (result == -1)
-					return mid;
-			}
-			//method call recursively for left sub array if the value is smaller than middle element
-			else if (arr[mid] > value) {
-				return leftMostOccurrence(arr, lower, mid - 1, value);
-			} 
-			//method call recursively for right sub array if the value is greater than middle element
-			else {
-				return leftMostOccurrence(arr, mid + 1, upper, value);
+				right = mid - 1;
+		}
+		return -1;
+	}
+	
+	int getLeftMostOccurence(int inputArray[], int left, int right, int key) {
+		int mid;
+		// Loop Invariant
+		// 1 right index-left index will always be greater than or equal to
+		// one
+		while ((right - left >= 1)) {
+			mid = left + (right - left) / 2;
+
+			if (inputArray[mid] >= key)
+				right = mid;
+			else
+				left = mid;
+			if(mid==0){
+				return right;
 			}
 		}
-		return result;
+		return right;
 	}
 
-	//method used to check the right most occurrence
-	public int rightMostOccurrence(int arr[], int lower, int upper, int value) {
-		int result = 0;
-		//returns if at any time lower bound is greater than upper bound
-		if (lower > upper) {
-			return result;             
-		}
-		//if the array is of size one than if the value of the element is similar than return its position
-		if (lower == upper) {
-			if (arr[lower] == value)
-				return lower;
+	
+	int getRightMostOccurence(int inputArray[], int left, int right, int key) {
+		int mid;
+		// Loop Invariant
+		// 1 right index-left index will always be greater than one
+		while (right - left > 1) {
+			mid = left + (right - left) / 2;
+
+			if (inputArray[mid] <= key)
+				left = mid;
 			else
-				return -1;
+				right = mid;
 		}
-		//else if lower bound is smaller than upper bound than perform following operations
-		else if (lower <= upper) {
-			//calculated middle element
-			int mid = (lower + upper) / 2;
-			//checks value at middle element 
-			if (arr[mid] == value) {
-				result = mid;
-				//if value is equal than it is recursively called for right sub array
-				result = rightMostOccurrence(arr, mid + 1, upper, value);
-				//if at any position value returned by the method is -1 than method finally gives the index of middle element
-				if (result == -1)
-					return mid;
-			}
-			//method call recursively for left sub array if the value is smaller than middle element
-			else if (arr[mid] > value) {
-				return rightMostOccurrence(arr, lower, mid - 1, value);
-			}
-			//method call recursively for right sub array if the value is greater than middle element
-			else {
-				return rightMostOccurrence(arr, mid + 1, upper, value);
-			}
-		}
-		return result;
-	}
 
-	//main function
-	public static void main(String[] args) {
-		//utility class contains function which checks the input string entered
-		System.out.println("Enter the size of array");
-		int size = Utility.checkInteger();
-		System.out.println("Enter the values in array");
-		int arr[] = new int[size];
-		for (int i = 0; i < size; i++) {
-
-			arr[i] = Utility.checkInteger();
-		}
-		System.out.println("Enter the value to be searched");
-		int value = Utility.checkInteger();
-		BinarySearch object = new BinarySearch();
-		do {
-			System.out
-			.println("Press\n1. For Left Most Occurence Of Character");
-			System.out.println("2. For Right Most Occurence Of Character");
-			System.out.println("3. Exit");
-			int choice = Utility.checkInteger();
-			switch (choice) {
-			case 1:
-				System.out.println("Left most position of element is"
-						+ object.leftMostOccurrence(arr, 0, arr.length, value));
-				break;
-			case 2:
-				System.out.println("Right most position of element is"
-						+ object.rightMostOccurrence(arr, 0, arr.length, value));
-				break;
-			case 3:
-				System.exit(0);
-
-			default:
-				System.out.println("Enter a valid value");
-			}
-
-		} while (true);
+		return left;
 	}
 }
