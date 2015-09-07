187460d345f2a20e6cacf116617475933df2ab8a
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-09-07 19:29:18 +0530

    Update FindOccurrence.java

--- DS/Assignment5/FindOccurrence.java
+++ DS/Assignment5/FindOccurrence.java
@@ -15,35 +15,26 @@ import java.util.Set;
 public class FindOccurrence {
 	HashMap<Character, String> hashMapObject = new HashMap<Character, String>(); 
 	public void findOccurrence(String inputString) {
-		int counter;
 		int stringLength = inputString.length();
-		String key = "";
-		//set for unique element
-		Set<Character> uniqueElement = new HashSet<Character>();
-		for(int loopCounter = 0 ; loopCounter < stringLength ; loopCounter++) {		//insert the elements in set
-			uniqueElement.add(inputString.charAt(loopCounter));
-		}
-		// insert element in hashmap without value
-		for(Character character : uniqueElement) {
-			hashMapObject.put(character, "");
-		}
-		String positions = "" ;
-		for(Character character : uniqueElement) {
-			for(int loopCounter = 0 ; loopCounter < stringLength ; loopCounter++) {
-				if(inputString.charAt(loopCounter) == character) {
-					positions += loopCounter + ", ";
-				}
+		char key ;
+		String keyString = "" ;
+
+		for(int count = 0; count < stringLength; count++) {
+			key = inputString.charAt(count);
+			keyString = "";
+			if(hashMapObject.containsKey(key)) {
+				keyString = hashMapObject.get(key);
+				keyString += ", "+ count;
+			} else {
+				keyString += count;
 			}
-			hashMapObject.replace(character, positions);
-			positions = "" ;
+			hashMapObject.put(key, keyString);
 		}
-		
 	}
-	
 	public void printOccurence() {
 		for(Map.Entry mapping:hashMapObject.entrySet()){  
-			   System.out.println(mapping.getKey()+" = "+mapping.getValue());  
-			  }  
+			System.out.println(mapping.getKey()+" = "+mapping.getValue());  
+		}  
 	}
 
 }
