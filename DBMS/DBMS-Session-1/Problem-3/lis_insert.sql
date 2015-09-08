0f8a7c7a27641706d027bd1b1f0adb76e3e75611
Banwari kevat <banwari.kevat@metacube.com>
2015-09-08 22:46:04 +0530

    Update lis_insert.sql

--- DBMS/DBMS-Session-1/Problem-3/lis_insert.sql
+++ DBMS/DBMS-Session-1/Problem-3/lis_insert.sql
@@ -1,7 +1,8 @@
+--select database
 USE lib_info_sys;
-
+ --set auto increment 
  ALTER TABLE members AUTO_INCREMENT=1;
- 
+ -- insert into members table
  INSERT INTO  members (member_nm , addressline1, addressline2 , category) VALUES ( 1 ,'Ramesh' , 'Gole Ka Mandir' , 'Gwalior' , 'GENERAL' );
  INSERT INTO  members (member_nm , addressline1, addressline2 , category) VALUES ('Bhupendra' , 'Ahnuman square' , 'Indore' , 'OBC' );
  INSERT INTO  members (member_nm , addressline1, addressline2 , category) VALUES ('Deepak' , 'Station raod' , 'Bangalore' , 'SC' );
@@ -10,7 +11,7 @@ USE lib_info_sys;
  INSERT INTO  members (member_nm , addressline1, addressline2 , category) VALUES ('Divya' , 'Focus' , 'Dehli' , 'OBC' );
  
  
-  
+ -- insert into books table
  INSERT INTO books VALUES ( 'SC23435', 1 , '2014-2-8' , 200, 'Y');
  INSERT INTO books VALUES ( 'SC67463', 3 , '2015-4-23' , 400, 'Y');
  INSERT INTO books VALUES ( 'SC23235', 4 , '2015-2-1' , 600, 'Y');
@@ -20,7 +21,7 @@ USE lib_info_sys;
  INSERT INTO books VALUES ( 'SC76399', 3 , '2015-2-18' , 100, 'Y');
  
  
-  
+ --insert into authors table 
  INSERT INTO authors values ( 1 , 'Shivnandan' );
  INSERT INTO authors values ( 2 , 'Kavita Bala' );
  INSERT INTO authors values ( 4 , 'Conway' );
@@ -32,14 +33,14 @@ USE lib_info_sys;
  INSERT INTO authors values ( 10 , 'Vedant' );
  
  
-  
+ --insert into publishers table  
  INSERT INTO publishers values ( 1 , 'Jai prakashan' );
  INSERT INTO publishers values ( 2 , 'Shivani prakshan' );
  INSERT INTO publishers values ( 3 , 'Soni Prakashan' );
  INSERT INTO publishers values ( 4 , 'Keshav publikeshion' );
  
  
-  
+ --insert into subjects table 
  INSERT INTO subjects values ( 1 , 'DBMS' );
  INSERT INTO subjects values ( 2 , 'DS' );
  INSERT INTO subjects values ( 3 , 'TOC' );
@@ -50,7 +51,7 @@ USE lib_info_sys;
  INSERT INTO subjects values ( 8 , 'CPP' );
  
  
-  
+ --insert into titels table 
  INSERT INTO titles VALUES(1,'Data Commun.', 5,1);
  INSERT INTO titles VALUES(2,'Data archesim.', 3,3);
  INSERT INTO titles VALUES(3,'Semicond.', 2,3);
@@ -60,7 +61,7 @@ USE lib_info_sys;
  INSERT INTO titles VALUES(7,'Auta meta.', 3,4);
  
 
-  
+ --insert into title_author table 
  INSERT INTO title_author values ( 5, 7 );
  INSERT INTO title_author values ( 6, 4 );
  INSERT INTO title_author values ( 7, 1 );
@@ -73,7 +74,7 @@ USE lib_info_sys;
  INSERT INTO title_author values ( 4, 9 );
  
  
- 
+ --insert into book_issue table
  INSERT INTO book_issue VALUES ('2015-8-29','SC23435',2,'2015-9-21'); 
  INSERT INTO book_issue VALUES ('2015-8-29','SC67463',2,'2015-11-29'); 
  INSERT INTO book_issue VALUES ('2015-9-4','SC23235',3,'2015-10-15'); 
@@ -81,7 +82,7 @@ USE lib_info_sys;
  INSERT INTO book_issue VALUES ('2015-9-7','SC76399',4,'2015-9-22'); 
 
 
-
+ --insert into book_return table
  INSERT INTO book_return VALUES ('2015-9-15','SC23435',2,'2015-8-29'); 
  INSERT INTO book_return VALUES ('2015-11-23','SC67463',2,'2015-8-29'); 
  INSERT INTO book_return VALUES ('2015-10-16','SC23235',3,'2015-9-4'); 
@@ -89,6 +90,6 @@ USE lib_info_sys;
  INSERT INTO book_return VALUES ('2015-9-29','SC76399',4,'2015-9-7'); 
 
 
-
+ --update addressline2
  UPDATE members SET addressline2 = 'Jaipur' where member_id = 2;
 
