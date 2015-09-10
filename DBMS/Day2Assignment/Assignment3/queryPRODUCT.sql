9046449f30671375e1875f5b3c9856918470c9d1
Ankit Tinker <nikhil.tinkar@metacube.com>
2015-09-10 18:22:59 +0530

    DBMS Assignment

---
+++ DBMS/Day2Assignment/Assignment3/queryPRODUCT.sql
@@ -0,0 +1,9 @@
+SELECT t.topcategoryname,s.supercategoryname,ifNull(c.categoryname,'-') as categoryname ,
+ifNull(sc.subcategoryname,'-') as subcategoryname
+FROM topcategory T Left outer join supercategory s on t.topcategory_ID=s.topcategory_ID 
+Left outer join category c on s.supercategory_ID=c.supercategory_ID 
+Left outer join subcategory sc on c.category_ID=sc.category_ID;
+ORDER by topcategoryname;
+
+
+SELECT * FROM topcategory;
