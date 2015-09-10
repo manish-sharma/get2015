82a7b0eac88e7296ed8c4039f31e59473da22343
Banwari kevat <banwari.kevat@metacube.com>
2015-09-10 12:29:48 +0530

    Update assignment1.sql

--- DBMS/DBMS-Session-2/Problem1/assignment1.sql
+++ DBMS/DBMS-Session-2/Problem1/assignment1.sql
@@ -1,35 +1,41 @@
-show databases;
-
+-- select datbase
 USE lib_info_sys;
-
+--Diplay all the columns of members table
 SELECT * FROM members;  
 
-SELECT member_nm ,member_id , category from members; 
+-- Display Member name , id and category
+SELECT member_nm "NAME" ,member_id "ID" , category "CATEGORY" from members; 
 
-SELECT member_nm ,member_id , category from members
+-- Display Member name , id  whose category should be professor
+SELECT member_nm "NAME" ,member_id "ID", category "CATEGORY" from members
 WHERE category = 'professor' ; 
 
+-- Display all categoris name
 SELECT DISTINCT category from members;
 
-SELECT member_nm , category from members 
+-- Display Member name and category
+-- Name will in descending order  
+SELECT member_nm "MEMBER NAME", category from members 
 ORDER BY member_nm DESC;
 
-SELECT title_nm , sb.subject_nm, pb.publisher_nm 
+-- Display All titles and their publishers and Subjects
+SELECT title_nm "BOOK TITLE", sb.subject_nm "SUBJECT NAME", pb.publisher_nm 
 FROM titles tt , subjects sb, publishers pb 
 WHERE tt.subject_id = sb.subject_id AND tt.publisher_id = pb.publisher_id;
 
-
-SELECT category, count(*) AS total_type FROM members
+-- Display number of members present in particular Category
+SELECT category ", count(*) "total_type" FROM members
 GROUP BY category;
-
+-- Diplay Members name whose category same to Keshav Sharma
 SELECT member_nm from members
 where category IN 
                   (
                     SELECT category from members
-                    WHERE member_nm = 'Deepak'
+                    WHERE member_nm = 'Keshav Sharma'
                   );
                   
  
+ -- Display  the information of all issued book
  SELECT bi.issue_dt , bi.accession_no , bi.member_id , br.return_dt 
  FROM book_issue bi LEFT JOIN book_return br
  ON bi.accession_no = br.accession_no;
