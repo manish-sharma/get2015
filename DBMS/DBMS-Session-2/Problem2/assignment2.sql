9dad6945e352c03e0251ce0c151d6206a4b5d99f
Banwari kevat <banwari.kevat@metacube.com>
2015-09-10 11:22:22 +0530

    Update assignment2.sql

--- DBMS/DBMS-Session-2/Problem2/assignment2.sql
+++ DBMS/DBMS-Session-2/Problem2/assignment2.sql
@@ -1,12 +1,7 @@
+-- select DATABASE 
  USE lib_info_sys;
- SELECT issue_dt , accession_no , temp_table.member_id , return_dt
- FROM members ,
-               ( SELECT bi.issue_dt , bi.accession_no , bi.member_id , br.return_dt 
-                 FROM book_issue bi LEFT JOIN book_return br
-                 ON bi.accession_no = br.accession_no
-               ) 
-               AS temp_table
- WHERE members.member_id = temp_table.member_id              
- ORDER BY issue_dt , member.member_nm;
- 
-
+ -- informations of all issued books
+ SELECT bi.issue_dt "ISSUE DATE" , bi.accession_no "ACCESSION NO" , bi.member_id "MEMBER ID ", br.return_dt "RETUEN DATE"
+ FROM book_issue bi LEFT JOIN book_return br ON bi.accession_no = br.accession_no
+ JOIN members m ON bi.member_id = m.member_id
+ ORDER BY bi.issue_dt , m.member_nm;
