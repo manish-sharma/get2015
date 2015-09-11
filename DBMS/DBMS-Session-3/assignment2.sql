253b8afdd5b6a8c598688c519632ae6e79c723a0
sanjay nainani <sanjay.nainani@metacube.com>
2015-09-11 14:56:53 +0530

    DBMS3

---
+++ DBMS/DBMS-Session-3/assignment2.sql
@@ -0,0 +1,13 @@
+-- assignment 2
+-- Question 1
+select subjects.subject_Name,subjects.subject_Id, count(subjects.subject_Id) as "number of books purchased" 
+ from subjects,books,title 
+ where subjects.subject_Id = title.subject_Id and books.title_id = title.title_Id
+ group by subjects.subject_Id;
+ 
+ -- Question 2
+ select * from book_Issue where datediff(due_date,issue_date) > 60;
+ 
+ 
+ -- Question 3
+ select title_id,accession_no from books where price > (select Min(Price) from books);
