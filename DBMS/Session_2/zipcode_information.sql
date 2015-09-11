a070aedef8f3c32d51dc2aafa277378b907a30ec
meta-neha-bansal <neha.bansal@metacube.com>
2015-09-11 10:42:57 +0530

    Update zipcode_information.sql

--- DBMS/Session_2/zipcode_information.sql
+++ DBMS/Session_2/zipcode_information.sql
@@ -7,6 +7,7 @@
  USE zipcode_information;
  
  
+ 
  /*
   * Creating Table Zip_info
   */
@@ -23,7 +24,8 @@
  CREATE TABLE city_info
  (
    city_id INT PRIMARY KEY,
-   city_nm VARCHAR(30)
+   city_nm VARCHAR(30),
+   state_id INT
    
  );
  
@@ -32,9 +34,8 @@
   */
  CREATE TABLE state_info
  (
-   state_id INT ,
-   state_nm VARCHAR(30),
-   city_id INT
+   state_id INT PRIMARY KEY,
+   state_nm VARCHAR(30)
    
  );
  
@@ -58,32 +59,26 @@
  /*
   * Inserting data in city_info
   */
- INSERT INTO city_info(city_nm,city_id)
+ INSERT INTO city_info(city_nm,city_id,state_id)
  VALUES
- ("Ahmedabad",1001),
- ("Gandhinagar",1002),
- ("Vadodara",1003),
- ("Jaipur" ,1004),
- ("Bikaner",1005),
- ("Ajmer",1006),
- ("Gwalior",1007),
- ("Indore",1008),
- ("Bhopal" ,1009);
+ ("Ahmedabad",1001,1),
+ ("Gandhinagar",1002,1),
+ ("Vadodara",1003,1),
+ ("Jaipur" ,1004,2),
+ ("Bikaner",1005,2),
+ ("Ajmer",1006,2),
+ ("Gwalior",1007,3),
+ ("Indore",1008,3),
+ ("Bhopal" ,1009,3);
  
  /*
   * Inserting data in state_info
   */
- INSERT INTO state_info(state_id,state_nm,city_id)
+ INSERT INTO state_info(state_id,state_nm)
  VALUES
- (1,"Gujarat",1001),
- (1,"Gujarat",1002),
- (1,"Gujarat",1003),
- (2,"Rajasthan",1004),
- (2,"Rajasthan",1005),
- (2,"Rajasthan",1006),
- (3,"Madhya Pradesh",1007),
- (3,"Madhya Pradesh",1008),
- (3,"Madhya Pradesh",1009);
+ (1,"Gujarat"),
+ (2,"Rajasthan"),
+ (3,"Madhya Pradesh");
  
  
  
@@ -97,8 +92,9 @@
  INNER JOIN city_info
  ON Zip_info.city_id=city_info.city_id
  INNER JOIN state_info
- ON city_info.city_id=state_info.city_id
+ ON city_info.state_id=state_info.state_id
  ORDER BY State_nm,city_nm;
+ 
  
  
  
