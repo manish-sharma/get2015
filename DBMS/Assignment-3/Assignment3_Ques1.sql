5fa06a97c482a3b1bc8fd0ebe716867cc484242c
meta-shishir-pareek <shishir.pareek@metacube.com>
2015-09-11 17:21:58 +0530

    Update Assignment3_Ques1.sql

--- DBMS/Assignment-3/Assignment3_Ques1.sql
+++ DBMS/Assignment-3/Assignment3_Ques1.sql
@@ -1,9 +1,9 @@
 Use lis;
 SELECT * from book_issue;
-UPDATE book_issue SET due_dt = 2015-11-11 where member_id = "M12567";
-UPDATE book_issue SET due_dt = "2015-12-10" where member_id = "M14867";
-UPDATE book_issue SET due_dt = "2015-10-13" where member_id = "M14234";
-UPDATE book_issue SET due_dt = "2015-12-12" where member_id = "M12768";
+UPDATE book_issue SET due_dt = "2015-11-11" where member_id = "1";
+UPDATE book_issue SET due_dt = "2015-12-10" where member_id = "2";
+UPDATE book_issue SET due_dt = "2015-10-13" where member_id = "3";
+UPDATE book_issue SET due_dt = "2015-12-12" where member_id = "5";
 SELECT m1.member_nm,m1.member_id,t1.title_nm,b1.accession_no,b1.issue_dt,
 b1.due_dt,FLOOR(DATEDIFF(b1.due_dt,b1.issue_dt)/30) AS Months FROM members m1 JOIN book_issue b1 ON
 m1.member_id = b1.member_id JOIN books b2 ON b1.accession_no = b2.accession_no JOIN titles t1
