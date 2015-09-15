bc6aa5705dbf99ea98314ee14ce3fec69bb9215f
sumitra godara <sumitragodara@users.noreply.github.com>
2015-09-15 14:30:25 +0530

    Create VehicleDetailPrinter.java

---
+++ VehicleDetailPrinter.java
@@ -0,0 +1,21 @@
+package com.Vehicle;
+/**
+ * this class use to print all the information of vehicle selected by user
+ * @author Sumitra
+ *
+ */
+public class VehicleDetailPrinter {
+	/**
+	 * this method return string type value of all the attribute of vehicle selected by user
+	 * @param vehicle
+	 * @return
+	 */
+	public static String printVehicleDetail( Vehicle vehicle ){
+		
+			 String result =vehicle.toString();
+			    		return result;
+		
+}
+       
+	
+}
