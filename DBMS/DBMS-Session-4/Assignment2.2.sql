9d3111f61ad8729b860b1fddf0c83aaa7cc5c66b
Banwari kevat <banwari.kevat@metacube.com>
2015-09-15 14:59:01 +0530

    Update Assignment2.2.sql

--- DBMS/DBMS-Session-4/Assignment2.2.sql
+++ DBMS/DBMS-Session-4/Assignment2.2.sql
@@ -7,3 +7,17 @@
   
   -- Display view
   SELECT * FROM member_view;
+  
+  -- Create view of members table contaning member name , member id and short category 
+  CREATE VIEW member_view1 AS
+  SELECT member_nm AS "Member Name", member_id AS "Member ID", 
+                                                              CASE category 
+                                                                WHEN 'Faculty' THEN 'F' 
+                                                                WHEN 'Student' THEN 'S'
+                                                                ELSE 'O'
+                                                              END   AS "Category"
+  FROM members;
+  
+  
+  -- Display view
+  SELECT * FROM member_view1;
