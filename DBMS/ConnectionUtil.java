a3b4947295907d1edda5d672a9b561d731736196
ravikajain <ravika.jain@metacube.com>
2015-09-16 18:54:54 +0530

    Create ConnectionUtil.java

---
+++ DBMS/ConnectionUtil.java
@@ -0,0 +1,40 @@
+package com.metacube.jdbc.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ * to get connection
+ * 
+ * @author Ravika
+ *@since9/16/2015
+ */
+public class ConnectionUtil {
+
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/Library";
+	private static final String USER = "root";
+	private static final String PASSWORD = "mysql";
+			
+	private Connection con = null;
+	
+	public Connection getConnection() {
+		
+		/* register driver */
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		/* open connection */
+		try {
+			con = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
