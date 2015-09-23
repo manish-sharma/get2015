c84a998452381139a03678fc40fb990d5b1ce605
Girdhari Agrawal <girdhari.agrawal@metacube.com>
2015-09-23 17:34:36 +0530

    Create VehiclePrinter.java

---
+++ CS/CS-2/VehiclePrinter.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Girdhari
+ * VehiclePrinter Class 
+ */
+
+/* Starting of VehiclePrinter class */
+public class VehiclePrinter 
+{
+	public static String displayVehicleSpecification(Vehicle vehicle) {
+		return vehicle.toString();
+	}
+}
+
+/* Ending of VehiclePrinter Class */
