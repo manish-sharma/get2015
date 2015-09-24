105a57138a4a48bd015d3727a1ca16c88ed5d51f
ravikajain <ravika.jain@metacube.com>
2015-09-24 18:10:39 +0530

    Create profile.cc

---
+++ Web/profile.cc
@@ -0,0 +1,202 @@
+body {
+width : 100%
+height : 800px;
+background-image : url('image1.jpg');
+background-size:100% 770px;
+background-repeat : no-repeat;
+
+}
+#header {
+	margin-top : 20px;
+	margin-left :20px;
+	margin-right:20px;
+	width : 100;
+	text-align : center;
+	height : 100px;
+	background : #DEB887;
+	font-size : 200%;
+}
+ 
+ #nav {
+	padding : 5px;
+	margin-left :20px;
+	margin-right:20px;	
+	background : #BDB76B;
+	height :30px;
+	width : 100;	
+ }
+ .item1 {
+
+	display:inline;	
+	border:1px solid #D9B9B9;
+	margin:10px 10px;
+	padding-top: 2px;
+	padding-bottom: 2px;
+	background : #A9A9A9;
+	text-align :center;
+ }
+ .item2 {
+
+	display:inline;	
+	margin:10px 10px;
+	padding-bottom : 10px;
+	text-align :center;
+ }
+ 
+ #contant {
+	 background : #FFFFFF;
+	 width : 100;
+	 height : 500px;
+	 margin-left :20px;
+	margin-right:20px;
+ }
+ #leftContainer{
+  float: left;
+  background: white;
+  width: 400px;
+  height: 500px;
+ }
+ 
+ #leftNav {
+	 margin-top:5px;
+	 margin-left : 5px;
+	 padding-top : 5px;
+	 border:1px solid #D9B9B9;
+	 background : #F0FFFf;
+	 height : 175px;
+		 width:100;
+
+
+ }
+ #rightContainer{
+  float: right;
+  background: white;
+  width: 400px;
+  height: 500px;
+ }
+
+ #menuHead {
+	 list-style :none;
+	 width:81%;
+	 font-weight : bold;
+	 margin-left : 5px;
+	 margin-right :5px;
+	 margin-top :5px;
+	border:1px solid #D9B9B9;
+	height : 15px;
+	text-align:left;
+	padding :5px;
+	background : #C3E4ED;
+ }
+ #menuHead1{
+
+	 width:81%;
+	 font-weight : bold;
+	 margin-left : 5px;
+	 margin-right :5px;
+	 margin-top :5px;
+	border:1px solid #D9B9B9;
+	height : 15px;
+	text-align:left;
+	padding :5px;
+	background : #C3E4ED;
+ }
+ .menuItem {
+	 list-style :none;
+	 width:81%;
+	 	height : 15px;
+	margin-top : 5px;
+	 margin-left :5px;
+	 margin-bottom:5px;
+	text-align:left;
+	padding :5px;
+	background : #C3E4ED;
+ }
+ .menuItem2 {
+	 list-style :none;
+	 width:81%;
+	 margin-left :5px;
+	height : 15px;
+
+	text-align:left;
+	padding :5px;
+	
+ }
+ #menuItem3 {
+	 list-style :none;
+	 width:81%;
+	 margin-left :5px;
+	height : 15px;
+
+	text-align:left;
+	padding :5px;
+	
+ }
+ #menuItem3 :HOVER  {
+	 color:black;
+	
+	
+ }
+ 
+ 
+ #center {
+	 margin-left:50px;
+	 margin-top :10px;
+	 width: 600px;
+	 height : 500px;
+	 float :left;
+	 
+ }
+ #rightNav {
+	 margin-top : 10px;
+	 padding-top : 5px;
+	margin-right :10px;
+	 border:1px solid #D9B9B9;
+	 background : #F0FFFf;
+	 height : 100px;
+		 width:100%;
+	 
+ }
+ #category{
+	 margin-top : 10px;
+	 padding-top : 5px;
+	margin-right :10px;
+	 border:1px solid #D9B9B9;
+	 background : #F0FFFf;
+	 height : 200px;
+		 width:100%;
+	 
+ }
+ #font1 {
+	 
+ }
+ #centerHeader {
+	margin-top : 5px;
+	margin-left :10px;
+	margin-right:5px;
+	width : 100;
+	text-align : left;
+	height : 75px;
+	font-size : 150%;
+}
+#searchBlock {
+	width:81%;
+	 font-weight : bold;
+	 margin-left : 5px;
+	 margin-right :5px;
+	 margin-top :5px;
+	height : 15px;
+	text-align:left;
+	padding :5px
+	
+}
+  #footer {
+	  padding-top : 30px;
+	  background:#DCDCDC;
+	  width : 100;
+	 height : 55px;
+	 margin-left :20px;
+	margin-right:20px;
+	text-align : center;
+	
+  }
