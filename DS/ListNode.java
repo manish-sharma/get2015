f5a94ec2fefae00599b8a86ef54f7b491211b199
sumitra godara <sumitragodara@users.noreply.github.com>
2015-10-05 15:53:14 +0530

    Create ListNode.java

---
+++ DS/ListNode.java
@@ -0,0 +1,32 @@
+package Com.AdvanceDS.Question1;
+
+
+public class ListNode {
+	int flag;
+	Object data;
+	ListNode next;
+	public int getFlag() {
+		return flag;
+	}
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public ListNode getNext() {
+		return next;
+	}
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+	@Override
+	public String toString() {
+		return "\n   ListNode [flag=" + flag + ", data=" + data + ", next=" + next
+				+ "]";
+	}
+	
+}
