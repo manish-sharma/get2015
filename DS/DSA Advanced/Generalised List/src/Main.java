968fd4d0da6b2669693b681a8d8a8753e2bfe5e5
Gaurav Saini <gaurav.saini@metacube.com>
2015-10-09 13:28:04 +0530

    DSA Advanced

---
+++ DS/DSA Advanced/Generalised List/src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = null;
+		String input, repeatChoice;
+		int choice, number;
+		try {
+			sc = new Scanner(System.in);
+			do{
+				System.out.println("\nEnter list in string representation\n{eg. (4,(5,8),(2,(3,5),1),7)}");
+				input = sc.next();
+				if(Validation.isExpression(input)) {
+					List list = new List(input);
+					do {
+						System.out.println("\nEnter choice :\n1. Traverse the list\n2. Find Max element in the list\n3. Find Sum of the elements\n4. Size of the list\n5. Structure Of list\n6. Find a number");
+						do {// Validation
+							System.out.println("Enter Positive Number");
+							while (!sc.hasNextInt()) {
+							System.out.println("That's not a number!Please enter again");
+							sc.next(); // this is important!
+							}
+							choice = sc.nextInt();
+							} while (choice <= 0);
+						
+						switch(choice) { 
+							case 1:
+								if(!list.isEmpty()) {
+									list.traverse(list.first);
+								}
+								else {
+									System.out.println("List is empty");
+								}
+								break;
+							case 2:
+								if(!list.isEmpty())
+									System.out.println("Max = " + list.max());
+								else
+									System.out.println("list is empty");
+								break;
+							case 3:
+								if(!list.isEmpty())
+									System.out.println("Sum = " + list.sum());
+								else
+									System.out.println("List is empty");
+								break;
+							case 4:
+								if(!list.isEmpty())
+									System.out.println("size = " + list.size());
+								else
+									System.out.println("List is empty");
+								break;
+							case 5:
+								if(!list.isEmpty())
+									System.out.println(list.first);
+								else
+									System.out.println("List is empty");
+							case 6:
+								System.out.println("Enter a number");
+								while (!sc.hasNextInt()) {
+									System.out.println("That's not a number!Please enter again");
+									sc.next(); // this is important!
+									}
+									number = sc.nextInt();
+								if(!list.isEmpty())
+									if(list.find(number)) {
+										System.out.println("Number is found");
+									}
+									else
+										System.out.println("Number is not found");
+								else
+									System.out.println("List is empty");
+								break;
+							default : 
+								System.out.println("Enter Correct Chocie");
+						}
+						System.out.println("\nDo you want to continue with the same list.\n Press y or Y for Yes and any other keyu for No");
+						repeatChoice = sc.next();
+						if(!(repeatChoice.charAt(0) == 'Y' || repeatChoice.charAt(0) == 'y')) {
+							break;
+						}
+					} while(true);
+				}
+				else {
+					System.out.println("Please provide valid List");
+				}
+				System.out.println("\nDo you want to provide another list\nPress Y or y for YES and any ither key for EXIT");
+				repeatChoice = sc.next();
+				if(!(repeatChoice.charAt(0) == 'Y' || repeatChoice.charAt(0) == 'y')) {
+					System.out.println("System Exit");
+					System.exit(0);
+				}
+			} while(true);
+		}
+		catch(Exception e) { 
+			System.out.println(e.getMessage());
+			sc.close();
+		}
+	}
+}
---
+++ DS/DSA Advanced/Puzzle1/src/Utility.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gaurav Saini
+ */
+public class Utility 
+{
+	
+	/** Method to get Integer value from the given
+	 * message.
+	 * @param message
+	 * @return number
+	 */
+	public static int getIntegerValue(String message)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		do
+		{
+			System.out.println(message);
+			int number;
+			String inputStr = sc.next();
+			if(isNumeric(inputStr) && (Integer.parseInt(inputStr)>0))
+			{
+				number=Integer.parseInt(inputStr);
+				return number;
+			}
+			else
+			{
+				System.out.println("Enter positive integer value");
+			}
+		}while(true);
+	}
+	
+	
+	/** Method to check whether he given input string
+	 * has integer values or not
+	 * @param input : input string
+	 * @return true if string contains only numeric data, false otherwise */
+	private static boolean isNumeric(String input) 
+	{
+		try 
+		{
+			Integer.parseInt(input);
+		} 
+		catch (NumberFormatException e) 
+		{
+			return false;
+		}
+		return true;	
+	}
+	
+	/** Method to get String input from user 
+	 * 
+	 * @param message
+	 * @return inputStr
+	 */
+	public static String getStringInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		String inputStr = sc.nextLine();
+		return inputStr;
+	}
+	
+	/** Method to get character input from user
+	 * 
+	 * @param message
+	 * @return input : first character of input string
+	 */
+	public static char getCharacterInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		char input = sc.next().charAt(0);
+		if((input == 'y' || input == 'Y') || (input == 'n' || input == 'N'))
+		{
+			return input;
+		}
+		else
+		{	
+			return getCharacterInput(message);
+		}
+	}
+	
+	/** Method to get array input
+	 * 
+	 * @param message
+	 * @return : integer array
+	 */
+	public static int[] getArrayInput(String message)
+	{
+		System.out.println(message);
+		Scanner sc=new Scanner(System.in);
+		int array[] = new int[9];
+		for(int i = 0; i < (array.length-1) ; i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		return array;
+	}
+	
+}
