21c3bf07a6bd74ee638a27216726960b9667eefc
Avinash-barola <Avinash barola>
2015-10-12 17:18:41 +0530

    My Assignments

--- AdvanceDS/Generalizedist/MyException.java
+++
@@ -1,9 +0,0 @@
-/*
- It is MyException class and it extend Exception class.
- */
-public class MyException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public MyException(Exception e) {
-		System.out.print("Entered List is wrong");
-	}
-}
--- AdvanceDS/TruthTable/MyException.java
+++
@@ -1,8 +0,0 @@
-package TruthTable;
-
-public class MyException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public MyException(Exception e) {
-		System.out.print("Input expression is wrong");
-	}
-}
