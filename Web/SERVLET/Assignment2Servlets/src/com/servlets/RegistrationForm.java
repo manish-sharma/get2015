65fe0b3e1c1396b47e14fb3cc79dcaaf59051f28
sanjaynainani <sanjay.nainani@metacube.com>
2015-10-13 18:49:30 +0530

    SANJAY-JSP-SERVLET-1

---
+++ Web/SERVLET/Assignment2Servlets/src/com/servlets/RegistrationForm.java
@@ -0,0 +1,73 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class RegistrationForm extends HttpServlet {
+
+
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		PrintWriter pw = res.getWriter();
+		String result = "";
+		boolean flag=true;
+		res.setContentType("text/html");
+		pw.println("This is servlet page.<br>");
+		if(req.getParameter("firstName") == "")
+		{
+			result += "First Name not entered<br>";
+			flag=false;
+		} 
+		
+		if(req.getParameter("lastName")== "") 
+		{
+			result += "Last Name not entered<br>";
+		    flag=false;
+		}
+		
+		if(req.getParameter("password").length() < 8 )
+		{
+			result += "Password Length should be greater than 8 <br>";
+		     flag=false;
+		}
+		
+		if(!req.getParameter("password").equalsIgnoreCase(req.getParameter("confirmPassword"))) 
+		{  
+			result += "Password doesn't match<br>";
+			flag=false;
+		}
+		
+		if(req.getParameter("states")=="")
+		{ 
+			result += "State is not entered<br>";
+			flag=false;
+		}
+		if(req.getParameter("cities")=="") 
+		{ 
+			result += "Cities is not entered<br>";
+			flag=false;
+		}
+		if(req.getParameter("address")=="")
+		{
+			result += "Address is not entered<br>";
+			flag=false;
+		}
+	   if( req.getParameter("age")=="")
+		{
+		   result += "Age is not entered<br>";
+		   flag=false;	
+		}
+	   req.setAttribute("result", result);
+	   RequestDispatcher rd = req.getRequestDispatcher("ValidationCheck");
+	   rd.forward(req, res);
+	}
+}	
