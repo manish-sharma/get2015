d80d936b62a2be8267c812c95f0caf78d650764a
unknown <ravikajain>
2015-10-16 18:29:21 +0530

    Submission of Annotation

--- DS/assignment8/student/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Ravika
- *
- */
-package student;
---
+++ Web/Assignment1/Annotation/src/AnnotationParser.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Method;
+
+
+/**
+ * Its is used to parse the annotation requestForEnhancement
+ * @author Ravika
+ * @since 10/16/2015
+ */
+public class AnnotationParser {
+	public void parse(Class<?> tempClass) throws Exception {
+
+		Method[] methods = tempClass.getMethods();
+
+		for (Method method : methods) {
+
+			if (method.isAnnotationPresent(RequestForEnhancement.class)) {
+				RequestForEnhancement test = method.getAnnotation(RequestForEnhancement.class);
+				int info = test.id();
+				String synopsis = test.synopsis();
+				if (1 == info) {
+					System.out.println("info is awesome!");
+					method.invoke(UseOfRequestForEnhancement.class.newInstance(), info, synopsis,	test.engineer(), test.date());
+				}
+			}
+
+		}
+	}
+}
+
