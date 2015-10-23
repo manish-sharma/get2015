67988eacb19b707da16099e44f633b72a8edd5f6
banwari kevat <banwari.kevat@metacube.com>
2015-10-23 16:31:46 +0530

    Design_pattern-session1 Added

---
+++ Web/DesignPattern-Session-1/Assignment2/model/Engineer.java
@@ -0,0 +1,13 @@
+/**
+ * This is the interface for Enginner
+ * @author Banwari Kevat
+ */
+package model;
+public interface Engineer{
+	
+   public void setName(String name);
+   public String getName();
+   public EngineerType getRole();
+   public String toString();
+
+}
