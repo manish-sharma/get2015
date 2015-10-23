cb6d636c8a13f291c7d0e8afb5c94b76413a65d0
sanjaynainani <sanjay.nainani@metacube.com>
2015-10-23 11:44:00 +0530

    MVC2

---
+++ Web/MVC-2 Annotations/AnnotationAssignment3/src/CanRun.java
@@ -0,0 +1,17 @@
+
+/**
+ * annotation witch has any no element
+ * @author Sanjay
+ *
+ */
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CanRun {
+
+}
