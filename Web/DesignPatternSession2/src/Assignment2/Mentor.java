a21c4019c033092928862f3211dfa0644bd3df0e
unknown <ravikajain>
2015-10-23 16:47:54 +0530

    DP2

---
+++ Web/DesignPatternSession2/src/Assignment2/Mentor.java
@@ -0,0 +1,22 @@
+package Assignment2;
+
+/**
+ * 
+ * @author Ravika
+ * @since 23/10/2015
+ */
+public class Mentor extends LeaveHandler {
+	
+	public Mentor(int day) {
+		this.day = day;
+	}
+
+	/**
+	 * @see Assignment2.LeaveHandler#leaveApprovalMessage()
+	 */
+	@Override
+	protected void leaveApprovalMessage() {
+		System.out.println("Leave approved by mentor");
+	}
+}
+ 
