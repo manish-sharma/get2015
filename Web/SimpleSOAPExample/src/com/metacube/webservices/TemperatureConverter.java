152cc8f2580aff49bf896ca6c7544b948a5d9776
ankur gupta <ankur.gupta@metacube.com>
2015-10-26 17:42:35 +0530

    web services ankur assignment 1 submission

---
+++ Web/SimpleSOAPExample/src/com/metacube/webservices/TemperatureConverter.java
@@ -0,0 +1,24 @@
+package com.metacube.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+/**
+ * Web service contain convert method that convert fahrenheit into celcius
+ * @author Ankur
+ * @since 26-10-2015
+ */
+@WebService
+public class TemperatureConverter {
+	/**
+	 * method to convert fahrenheit into celcius
+	 * @param farenheit
+	 * @return converted value that is celcius value
+	 */
+	@WebMethod
+	public double convert(double farenheit) {
+		
+		return (farenheit - 32) * 5/9;
+
+	}
+
+}
---
+++ Web/TemperatureConverterClient/src/com/metacube/WSClient.java
@@ -0,0 +1,27 @@
+package com.metacube;
+
+import com.metacube.webservices.TemperatureConverter;
+import com.metacube.webservices.TemperatureConverterServiceLocator;
+/**
+ * client call web service from server
+ * @author Ankur
+ * @since 25-10-2015
+ */
+public class WSClient {
+
+	public static void main(String[] args) {
+		
+		TemperatureConverterServiceLocator calcCTCWebServiceImplServiceLocator = new TemperatureConverterServiceLocator();
+		calcCTCWebServiceImplServiceLocator.setTemperatureConverterEndpointAddress("http://localhost:8080/SimpleSOAPExample/services/TemperatureConverter");
+		
+		try {
+			TemperatureConverter calcCTCWebService = calcCTCWebServiceImplServiceLocator.getTemperatureConverter();
+			
+			System.out.println(calcCTCWebService.convert(10));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
