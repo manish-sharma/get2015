798cb80ad5862290b609dbe15db59d8767aa2d7e
meta-khemanshu-rao <khemanshu.rao@metacube.com>
2015-11-02 18:00:57 +0530

    assignment 5

---
+++ Web/JqueryAssignment1/Assignment2/script.js
@@ -0,0 +1,12 @@
+
+			function showHide() {
+				if ($('#Details').is(":visible")) {
+						$('#Details').hide();
+						$('#showHideButton').val('SHOW');
+				} else {
+						$('#Details').show();
+						$('#showHideButton').val('HIDE');
+					}
+			}
+		
+	
---
+++ Web/JqueryAssignment1/Assignment4/script.js
@@ -0,0 +1,7 @@
+
+		
+$(document).ready(function(){
+$("#grid").css({"width":"100%","text-align":"center"});
+$("#grid tr:even").css({"background-color":"#393ED4"});
+$("#grid tr:odd").css("background-color", "#50D439");
+});
