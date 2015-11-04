2c3692672a5ba0c9e17206e5fee5fc52d9ac5662
ravikajain <ravika.jain@metacube.com>
2015-11-04 19:11:59 +0530

    Update bufferData.js

--- Web/Bootstrap-session2/WebContent/js/bufferData.js
+++ Web/Bootstrap-session2/WebContent/js/bufferData.js
@@ -7,39 +7,39 @@ function loadData(){
 var contentArray=[];
 
 
-var movie1 = {Title:"ABCD", Genere:"Romantic", Director:"Amit", Review  : "Good", Rating : "Best ever"};
+var movie1 = {Title:"ABCD", Genere:"Romantic", Director:"Ravika", Review  : "Good", Rating : "Best ever"};
 
-var movie2 = {Title:"ABCD2", Genere:"Thriller", Director:"Jignesh", Review  : "Good", Rating : "Best ever"};
+var movie2 = {Title:"ABCD2", Genere:"Thriller", Director:"Sumitra", Review  : "Good", Rating : "Best ever"};
 
-var movie3 = {Title:"Golmaal", Genere:"Drama", Director:"Ankur", Review  : "Good", Rating : "Best ever"};
+var movie3 = {Title:"Golmaal", Genere:"Drama", Director:"Pooja", Review  : "Good", Rating : "Best ever"};
 
-var movie4 = {Title:"Golmaal returns", Genere:"Comedy", Director:"Anurag", Review  : "Good", Rating : "Best ever"};
+var movie4 = {Title:"Golmaal returns", Genere:"Comedy", Director:"Ekta", Review  : "Good", Rating : "Best ever"};
 
-var movie5 = {Title:"Sholey", Genere:"Thriller", Director:"Nikhil", Review  : "Good", Rating : "Best ever"};
+var movie5 = {Title:"Sholey", Genere:"Thriller", Director:"Sonam", Review  : "Good", Rating : "Best ever"};
 
-var movie6 = {Title:"Pyar ka Panchnama", Genere:"Horror", Director:"Rohit", Review  : "Good", Rating : "Best ever"};
+var movie6 = {Title:"Pyar ka Panchnama", Genere:"Horror", Director:"Priti", Review  : "Good", Rating : "Best ever"};
 
-var movie7 = {Title:"DDLJ", Genere:"Romantic", Director:"Khemanshu", Review  : "Good", Rating : "Best ever"};
+var movie7 = {Title:"DDLJ", Genere:"Romantic", Director:"Ajay", Review  : "Good", Rating : "Best ever"};
 
-var movie8 = {Title:"MP3", Genere:"Romantic", Director:"Girdhari", Review  : "Good", Rating : "Best ever"};
+var movie8 = {Title:"MP3", Genere:"Romantic", Director:"Ritika", Review  : "Good", Rating : "Best ever"};
 
-var movie9 = {Title:"Wanted", Genere:"Action", Director:"Sanjay", Review  : "Good", Rating : "Best ever"};
+var movie9 = {Title:"Wanted", Genere:"Action", Director:"Mona", Review  : "Good", Rating : "Best ever"};
 
-var movie10 = {Title:"Ready", Genere:"Comedy-Action", Director:"Pulkit", Review  : "Good", Rating : "Best ever"};
+var movie10 = {Title:"Ready", Genere:"Comedy-Action", Director:"Avinash", Review  : "Good", Rating : "Best ever"};
 
-var movie11 = {Title:"PK", Genere:"Drama", Director:"Akshat", Review  : "Good", Rating : "Best ever"};
+var movie11 = {Title:"PK", Genere:"Drama", Director:"Sumit", Review  : "Good", Rating : "Best ever"};
 
-var movie12 = {Title:"Dhoom", Genere:"Action", Director:"Subhash", Review  : "Good", Rating : "Best ever"};
+var movie12 = {Title:"Dhoom", Genere:"Action", Director:"Amit", Review  : "Good", Rating : "Best ever"};
 
-var movie13 = {Title:"Dhoom2", Genere:"Action", Director:"Gaurav", Review  : "Good", Rating : "Best ever"};
+var movie13 = {Title:"Dhoom2", Genere:"Action", Director:"Vijay", Review  : "Good", Rating : "Best ever"};
 
-var movie14 = {Title:"Dhoom3", Genere:"Action", Director:"Dheeraj", Review  : "Good", Rating : "Best ever"};
+var movie14 = {Title:"Dhoom3", Genere:"Action", Director:"Atul", Review  : "Good", Rating : "Best ever"};
 
-var movie15 = {Title:"Dhoom4", Genere:"Action", Director:"Macontennish", Review  : "Good", Rating : "Best ever"};
+var movie15 = {Title:"Dhoom4", Genere:"Action", Director:"Rishabh", Review  : "Good", Rating : "Best ever"};
 
-var movie16 = {Title:"Dhoom5", Genere:"Action", Director:"Banwari", Review  : "Good", Rating : "Best ever"};
+var movie16 = {Title:"Dhoom5", Genere:"Action", Director:"Surbhi", Review  : "Good", Rating : "Best ever"};
 
-var movie17 = {Title:"Dhoom6", Genere:"Action", Director:"Tinkar", Review  : "Good", Rating : "Best ever"};
+var movie17 = {Title:"Dhoom6", Genere:"Action", Director:"Bulbul", Review  : "Good", Rating : "Best ever"};
 
 contentArray.push(movie1);
 contentArray.push(movie2);
