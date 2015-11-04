9b84760df327fc64f5a649c7548892a3dc3bcfe3
Dheeraj Kumar <dheeraj.kumar1@metacube.com>
2015-11-04 18:58:54 +0530

    Bootstrap assignment 1 completed

--- Web/WebService-REST-Assignment/RestWebClient/dbscripts/20151102create.sql
+++
@@ -1,8 +0,0 @@
-create database Employee_management;
-use Employee_management;
-create table Employee(
-id int auto_increment primary key ,
-name varchar(30),
-email varchar(100),
-address varchar(200)
-);
